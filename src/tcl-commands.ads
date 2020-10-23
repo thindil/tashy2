43a7b067d697c431538bdac1a29d22964bb26d37
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-10-23 05:28:48 +0000

    added code documentation
    FossilOrigin-Name: f43d823b8323219cbd65e2c616f88d1e2a01c3517387f0cfef062bebee2dedb2

--- src/tcl-commands.ads
+++ src/tcl-commands.ads
@@ -17,30 +17,100 @@ with Interfaces.C.Pointers;
 with Interfaces.C.Strings; use Interfaces.C.Strings;
 with System; use System;
 
+-- ****h* Tcl/Commands
+-- FUNCTION
+-- Provide code to manipulate Tcl commands (adding, deleting, etc)
+-- SOURCE
 package Tcl.Commands is
+-- ****
 
+   -----------------------------
+   -- Creating a new Tcl command
+   -----------------------------
+
+   -- ****t* Commands/Argv_Pointer
+   -- FUNCTION
+   -- Used to store arguments of the selected Tcl command
+   -- SOURCE
    package Argv_Pointer is new Interfaces.C.Pointers(Index => size_t,
       Element => chars_ptr, Element_Array => chars_ptr_array,
       Default_Terminator => Null_Ptr);
+   -- ****
 
+   -- ****t* Commands/Tcl_CmdProc
+   -- FUNCTION
+   -- The Ada code which will be executed when the selected command will be
+   -- invoked.
+   -- PARAMETERS
+   -- CliendData  - Generally unused
+   -- Interpreter - The Tcl interpreter on which the command was invoked
+   -- Argc        - The number of arguments passed to the command. Minimum one.
+   -- Argv        - The pointer to the list of arguments passed to the command.
+   --               The first element is always the name of invoked procedure
+   -- RESULT
+   -- Should be one of TCL_OK, TCL_ERROR, TCL_RETURN, TCL_BREAK, TCL_CONTINUE
+   -- SOURCE
    type Tcl_CmdProc is access function
      (ClientData: System.Address; Interpreter: Tcl_Interpreter; Argc: Positive;
       Argv: Argv_Pointer.Pointer) return Natural with
       Convention => C;
+      -- ****
 
+      -- ****t* Commands/Tcl_CmdDeleteProc
+      -- FUNCTION
+      -- The Ada code which will be executed when the selected command will be
+      -- deleted.
+      -- PARAMETERS
+      -- ClientData - Generally unused
+      -- SOURCE
    type Tcl_CmdDeleteProc is access procedure(ClientData: System.Address) with
       Convention => C;
+      -- ****
 
+      -- ****t* Commands/Tcl_Command
+      -- FUNCTION
+      -- The pointer to a Tcl command
+      -- SOURCE
    type Tcl_Command is new System.Address;
+   -- ****
 
+   -- ****f* Commands/Tcl_CreateCommand
+   -- FUNCTION
+   -- Add a new Tcl command to the selected Tcl interpreter
+   -- PARAMETERS
+   -- Command_Name - The name of the Tcl command to add
+   -- Proc         - The code of the Tcl command to add
+   -- Interpreter  - The Tcl interpreter to which the command will be added.
+   --                By default it is the default Tcl interpreter
+   -- DeleteProc   - The code which will be executed on deleting Tcl command.
+   --                Default value is null
+   -- RESULT
+   -- The pointer to the newly created Tcl command
+   -- SOURCE
    function Tcl_CreateCommand
      (Command_Name: String; Proc: Tcl_CmdProc;
       Interpreter: Tcl_Interpreter := Get_Interpreter;
       DeleteProc: Tcl_CmdDeleteProc := null) return Tcl_Command with
       Pre => Command_Name'Length > 0,
       Test_Case => ("Test_Tcl_CreateCommand", Nominal);
+      -- ****
 
+      -----------------------------------------
+      -- Manipulating a Tcl command arguments
+      -----------------------------------------
+
+      -- ****f* Commands/Get_Argument
+      -- FUNCTION
+      -- Get the selected argument from the arguments list
+      -- PARAMETERS
+      -- Arguments_Pointer - C pointer to the list of arguments from which the
+      --                     argument will be taken
+      -- Index             - Index of the argument to take
+      -- RESULT
+      -- The selected argument from the list
+      -- SOURCE
    function Get_Argument
      (Arguments_Pointer: Argv_Pointer.Pointer; Index: Natural) return String;
+     -- ****
 
 end Tcl.Commands;
