059e634d565a5ff7958b51d3befbda153eb648a1
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-10-24 05:47:02 +0000

    started work on Tk binding
    FossilOrigin-Name: 805727f01f551a02b46e58449a3d604e84f854b75b1b2c3fd7cf4be557c02ab1

---
+++ src/tk/tk.adb
@@ -0,0 +1,30 @@
+-- Copyright (c) 2020 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+with Interfaces.C; use Interfaces.C;
+
+package body Tk is
+
+   procedure Tk_Init(Interpreter: Tcl_Interpreter := Get_Interpreter) is
+      function TkInit(interp: Tcl_Interpreter) return int with
+         Import => True,
+         Convention => C,
+         External_Name => "Tk_Init";
+   begin
+      if TkInit(Interpreter) = int(TCL_ERROR) then
+         raise Tcl_Exception with Tcl_GetStringResult;
+      end if;
+   end Tk_Init;
+
+end Tk;
