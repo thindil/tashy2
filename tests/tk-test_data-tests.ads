ee57dc1ef24ce05b58bb99e980b918270d20f5dd
thindil@laeran.pl <thindil@laeran.pl>
2020-10-25 06:07:12 +0000

    added unit tests
    FossilOrigin-Name: d6291d4583af9e7930873f1f969f2d9b90b9d177f57e03d647562f0040b188c1

---
+++ tests/tk-test_data-tests.ads
@@ -0,0 +1,16 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Tk.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Tk.Test_Data.Test with
+   null record;
+
+   procedure Test_Tk_Init_dae4dd_f1b9f1(Gnattest_T: in out Test);
+   --  tk.ads:40:4:Tk_Init:Test_Tk_Init
+
+end Tk.Test_Data.Tests;
+--  end read only
