bda7be25f117a32d8744a515bb9d4136da5c3612
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-11-01 06:56:00 +0000

    updated demo program
    FossilOrigin-Name: f1c998343afd1c905d73ff624b4373914f89cec08483446540b8dfdb0c21b829

--- demos/src/helloworld.adb
+++ demos/src/helloworld.adb
@@ -1,11 +1,21 @@
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Text_IO;
 with Tcl; use Tcl;
+with Tk; use Tk;
+with Tk.Button; use Tk.Button;
 
 procedure HelloWorld is
+   Button: Tk_Button;
+   ButtonConfig: constant Button_Options := (Ttk => False, Text => To_Unbounded_String("Click me"), others => <>);
 begin
-   -- Initialize the Tcl binding
+   -- Initialize Tcl interpreter
    Tcl_Init(Create_Interpreter);
+   -- Initialize the Tk library
+   Tk_Init;
 
-   -- Execute Tcl command on default Tcl interpreter
-   Tcl_Eval("puts {hello world from Ada}");
+   -- Create button
+   Button := Create(".button", ButtonConfig);
+
+   Ada.Text_IO.Put_Line(Integer'Image(Button.Tk_Window));
 
 end HelloWorld;
