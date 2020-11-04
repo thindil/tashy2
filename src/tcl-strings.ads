db2f0e430f19b635aefe925682d594b6bb1a084e
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-11-04 08:38:28 +0000

    added code documentation
    FossilOrigin-Name: 024080bcc7a997027536e41db669a7a9507963d69924de13ccbeabcfaa3d5832

--- src/tcl-strings.ads
+++ src/tcl-strings.ads
@@ -14,18 +14,41 @@
 
 with Ada.Strings.Unbounded;
 
+-- ****h* Tcl/Strings
+-- FUNCTION
+-- Provide code to manipulate the Tcl strings.
+-- SOURCE
 package Tcl.Strings is
+-- ****
 
    -- ****t* Strings/Strings.Tcl_String
    -- FUNCTION
    -- Used to store Tcl string values. Strings in Tcl can be evaluated or
    -- literal.
+   -- HISTORY
+   -- 8.6.0 - Added
    -- SOURCE
    type Tcl_String is new Ada.Strings.Unbounded.Unbounded_String;
    -- ****
 
+   -- ****f* Strings/Strings.To_Tcl_String
+   -- FUNCTION
+   -- Convert simple Ada String to Tcl_String and format it to evaluation
+   -- or as literal string
+   -- PARAMETERS
+   -- Source   - Ada String which will be converted to Tcl_String
+   -- Evaluate - If true, Source should be formatted to be evaluated as a
+   --            Tcl code (variables replaced by values, etc). Otherwise
+   --            format Source to be a literal string
+   -- RESULT
+   -- A new Tcl_String properly formatted to be evaluated or as literal
+   -- string
+   -- HISTORY
+   -- 8.6.0 - Added
+   -- SOURCE
    function To_Tcl_String
      (Source: String; Evaluate: Boolean := False) return Tcl_String with
       Pre => Source'Length > 0;
+      -- ****
 
 end Tcl.Strings;
