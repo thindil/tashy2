87954d73c0cac8947b0893eb0753652f2ac73e0c
thindil@laeran.pl <thindil@laeran.pl>
2020-11-21 06:27:58 +0000

    added unit test for Tcl_SerVar
    FossilOrigin-Name: d25179479e6d6b9446d59c2b4777e4195b3f1aaa0620bd2a1297a4953761c099

--- tests/tcl-test_data-tests.ads
+++ tests/tcl-test_data-tests.ads
@@ -19,5 +19,23 @@ package Tcl.Test_Data.Tests is
    procedure Test_Tcl_SetResult_e87522_5a72b9(Gnattest_T: in out Test);
    --  tcl.ads:232:4:Tcl_SetResult:Test_Tcl_SetResult
 
+   procedure Test_Tcl_SetVar_43dc1c_572e53(Gnattest_T: in out Test);
+   --  tcl.ads:282:4:Tcl_SetVar:Test_Tcl_SetVar
+
+   procedure Test_Tcl_SetVar2_38fd66_e29f6c(Gnattest_T: in out Test);
+   --  tcl.ads:310:4:Tcl_SetVar2:Test_Tcl_SetVar2
+
+   procedure Test_Tcl_GetVar_61051d_45f6e9(Gnattest_T: in out Test);
+   --  tcl.ads:338:4:Tcl_GetVar:Test_Tcl_GetVar
+
+   procedure Test_Tcl_GetVar2_7938c0_601542(Gnattest_T: in out Test);
+   --  tcl.ads:418:4:Tcl_GetVar2:Test_Tcl_GetVar2
+
+   procedure Test_Tcl_UnsetVar_458887_5d27f9(Gnattest_T: in out Test);
+   --  tcl.ads:505:4:Tcl_UnsetVar:Test_Tcl_UnsetVar
+
+   procedure Test_Tcl_UnsetVar2_5c4efc_618eb3(Gnattest_T: in out Test);
+   --  tcl.ads:531:4:Tcl_UnsetVar2:Test_Tcl_UnsetVar2
+
 end Tcl.Test_Data.Tests;
 --  end read only
