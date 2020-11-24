87abebb4ecdeb44588c3f951d3ef9fb80de99588
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-11-24 06:19:53 +0000

    updated demo program
    FossilOrigin-Name: 3e61e05b42c2b215ca323af407dfb9c56ac32ffdab58cd71d338a5ec17445508

--- demos/src/helloworld.adb
+++ demos/src/helloworld.adb
@@ -2,6 +2,7 @@ with Tcl; use Tcl;
 with Tcl.Strings; use Tcl.Strings;
 with Tk; use Tk;
 with Tk.Button; use Tk.Button;
+with Tk.Grid; use Tk.Grid;
 
 procedure HelloWorld is
    Button: Tk_Button;
@@ -18,7 +19,7 @@ begin
       (Text => To_Tcl_String("Quit"), Command => To_Tcl_String("exit"),
        others => <>));
    -- Add the button to the main window
-   Tcl_Eval("grid " & Tk_PathName(Button));
+   Add(Button);
 
    -- Start the main Tk event loop
    Tk_MainLoop;
