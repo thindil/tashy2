0e89a452218a23f24afc6c1cee27226e774200dd
thindil@laeran.pl <thindil@laeran.pl@noemail.net>
2020-11-26 06:33:06 +0000

    started work on converting options to string
    FossilOrigin-Name: 555eca527c43d0e900fc717633645f30d91fd080d0bdc226feb881a4122ab1e4

--- src/tk/tk-grid.adb
+++ src/tk/tk-grid.adb
@@ -12,11 +12,39 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
 package body Tk.Grid is
 
-   procedure Add(Widget: Tk_Widget; Options: Grid_Options := Grid_Options'(others => <>)) is
+   -- ****if* Grid/Grid.Options_To_String
+   -- FUNCTION
+   -- Convert Ada structure to Tcl command
+   -- PARAMETERS
+   -- Options - Ada Grid_Options to convert
+   -- RESULT
+   -- String with Tcl command options
+   -- HISTORY
+   -- 8.6.0 - Added
+   -- SOURCE
+   function Options_To_String(Options: Grid_Options) return String is
+      -- ****
+      Options_String: Unbounded_String;
    begin
-      Tcl_Eval("grid " & Tk_PathName(Widget), Tk_Interp(Widget));
+      Option_Image("column", Options.Column, Options_String);
+      Option_Image("columnspan", Options.ColumnSpan, Options_String);
+      Option_Image("in", Options.In_Master, Options_String);
+      Option_Image("ipadx", Options.IPadX, Options_String);
+      Option_Image("ipady", Options.IPadY, Options_String);
+      return To_String(Options_String);
+   end Options_To_String;
+
+   procedure Add
+     (Widget: Tk_Widget;
+      Options: Grid_Options := Grid_Options'(others => <>)) is
+   begin
+      Tcl_Eval
+        ("grid " & Tk_PathName(Widget) & Options_To_String(Options),
+         Tk_Interp(Widget));
    end Add;
 
 end Tk.Grid;
