0e6fed50bb96d4c50ed33ba03f02002a40b94cc1
thindil@laeran.pl <thindil@laeran.pl>
2020-11-27 06:30:29 +0000

    updated unit tests
    FossilOrigin-Name: ead6749b5a89e886347de33f2948b999bc6e3e7460ac6daca2a49557fbbc0d84

--- tests/tk-button-test_data-tests.ads
+++ tests/tk-button-test_data-tests.ads
@@ -1,2 +1,14 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
 package Tk.Button.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Tk.Button.Test_Data
+     .Test with
+   null record;
+
 end Tk.Button.Test_Data.Tests;
+--  end read only
