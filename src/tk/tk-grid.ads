1f275c76956a8a1298f2782de883b5984ca1a63d
thindil@laeran.pl <thindil@laeran.pl>
2020-11-30 06:59:28 +0000

    removed unneeded configure binding
    FossilOrigin-Name: 74b100bde945eff43aeec45fda03bfca000ada7558c3617fc051bf670fca4906

--- src/tk/tk-grid.ads
+++ src/tk/tk-grid.ads
@@ -78,9 +78,6 @@ package Tk.Grid is
    procedure Configure(Widgets: Widgets_Array; Options: Grid_Options) with
       Pre => Widgets'Length > 0 and Options /= Grid_Options'(others => <>);
 
-   procedure Configure(Widgets: String; Options: Grid_Options) with
-      Pre => Widgets'Length > 0 and Options /= Grid_Options'(others => <>);
-
    function Get_Options(Widget: Tk_Widget) return Grid_Options with
       Pre => Widget /= Null_Widget;
 
