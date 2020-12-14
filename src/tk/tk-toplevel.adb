e3771733d7020e66c7fb514714378bcf32e965e5
thindil <thindil@noemail.net>
2020-12-14 07:05:18 +0000

    started work on converting Toplevel_Create_Options to string
    FossilOrigin-Name: 2546d3b3044f5e4e3ecf5782715cd62bc3f55c94285736173b49001758b4bd5f

--- src/tk/tk-toplevel.adb
+++ src/tk/tk-toplevel.adb
@@ -12,7 +12,48 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
 package body Tk.TopLevel is
+
+   -- ****if* TopLevel/TopLevel.Options_To_String
+   -- FUNCTION
+   -- Convert Ada structure to Tcl command
+   -- PARAMETERS
+   -- Options - Ada TopLevel_Create_Options to convert
+   -- RESULT
+   -- String with Tcl command options
+   -- HISTORY
+   -- 8.6.0 - Added
+   -- SOURCE
+   function Options_To_String
+     (Options: TopLevel_Create_Options) return String is
+      -- ****
+      Options_String: Unbounded_String;
+   begin
+      Option_Image("background", Options.Background, Options_String);
+      Option_Image("borderwidth", Options.Border_Width, Options_String);
+      Option_Image("class", Options.Class, Options_String);
+      Option_Image("colormap", Options.Color_Map, Options_String);
+      Option_Image("container", Options.Container, Options_String);
+      Option_Image("cursor", Options.Cursor, Options_String);
+      Option_Image("height", Options.Height, Options_String);
+      Option_Image
+        ("highlightbackground", Options.Highlight_Background, Options_String);
+      Option_Image("highlightcolot", Options.Highlight_Color, Options_String);
+      Option_Image
+        ("highlighthickness", Options.Highlight_Thickness, Options_String);
+      Option_Image("menu", Options.Menu, Options_String);
+      Option_Image("padx", Options.PadX, Options_String);
+      Option_Image("pady", Options.PadY, Options_String);
+      Option_Image("relief", Options.Relief, Options_String);
+      Option_Image("screen", Options.Screen, Options_String);
+      Option_Image("takefocus", Options.Take_Focus, Options_String);
+      --Option_Image("use", Options.Use_Container, Options_String);
+      Option_Image("visual", Options.Visual, Options_String);
+      Option_Image("width", Options.Width, Options_String);
+      return To_String(Options_String);
+   end Options_To_String;
 
    function TopLevel_New
      (Path_Name: String; Options: TopLevel_Create_Options;
@@ -24,7 +65,8 @@ package body Tk.TopLevel is
    end TopLevel_New;
 
    procedure TopLevel_New
-     (Widget: out Tk_TopLevel; Path_Name: String; Options: TopLevel_Create_Options;
+     (Widget: out Tk_TopLevel; Path_Name: String;
+      Options: TopLevel_Create_Options;
       Interpreter: Tcl_Interpreter := Get_Interpreter) is
    begin
       Widget := TopLevel_New(Path_Name, Options, Interpreter);
