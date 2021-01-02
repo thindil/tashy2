89bfcf502e207381db491a41ccba194063ef330b
thindil <thindil@noemail.net>
2021-01-02 10:59:01 +0000

    updated unit tests
    FossilOrigin-Name: b3a23bd421fb5071d578c2cfa394a44caaa6bac9bee34657f91df9af9b9cde7e

---
+++ tests/tcl-info-test_data-tests.ads
@@ -0,0 +1,17 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Tcl.Info.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Tcl.Info.Test_Data
+     .Test with
+   null record;
+
+   procedure Test_Exists_e0ba6a_a2ca41(Gnattest_T: in out Test);
+   --  tcl-info.ads:34:4:Exists:Test_Info_Exists
+
+end Tcl.Info.Test_Data.Tests;
+--  end read only
