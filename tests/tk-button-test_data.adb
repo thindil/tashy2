d3e3c8a03dfb544ca316a87c58e9b53ddf0852a6
thindil <thindil@laeran.pl>
2021-01-02 10:56:12 +0000

    added setting Tk for button tests
    FossilOrigin-Name: 1fb26d748ae2d2a4909d3c7aec7e2b95b2d93f92bb23d531a5543409a591e8a3

--- tests/tk-button-test_data.adb
+++ tests/tk-button-test_data.adb
@@ -3,12 +3,18 @@
 --  automatically. Contents of this package can be modified in any way
 --  except for sections surrounded by a 'read only' marker.
 
+with Ada.Environment_Variables; use Ada.Environment_Variables;
+with Tcl.Info; use Tcl.Info;
+
 package body Tk.Button.Test_Data is
 
    procedure Set_Up(Gnattest_T: in out Test) is
       pragma Unreferenced(Gnattest_T);
    begin
-      null;
+      if Value("DISPLAY", "")'Length = 0 or else Info.Exists("tk_version") then
+         return;
+      end if;
+      Tk_Init;
    end Set_Up;
 
    procedure Tear_Down(Gnattest_T: in out Test) is
