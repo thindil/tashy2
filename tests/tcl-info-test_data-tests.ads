981a519c820cd561e099bf3b3f0596080c1391e3
thindil <thindil@laeran.pl>
2021-01-16 06:45:23 +0000

    updated unit tests
    FossilOrigin-Name: 00390700d2b4ed36e60e5cfe880dfecfb1b3e7b7e5f8939723353af7d60d0be7

--- tests/tcl-info-test_data-tests.ads
+++ tests/tcl-info-test_data-tests.ads
@@ -10,8 +10,65 @@ package Tcl.Info.Test_Data.Tests is
      .Test with
    null record;
 
+   procedure Test_Arguments_d24517_2155d3(Gnattest_T: in out Test);
+   --  tcl-info.ads:46:4:Arguments:Test_Info_Arguments
+
+   procedure Test_Procedure_Body_bc2330_b86b33(Gnattest_T: in out Test);
+   --  tcl-info.ads:68:4:Procedure_Body:Test_Info_Procedure_Body
+
+   procedure Test_Commands_Count_39e46a_cae496(Gnattest_T: in out Test);
+   --  tcl-info.ads:90:4:Commands_Count:Test_Info_Commands_Count
+
+   procedure Test_Commands_ff86c2_675616(Gnattest_T: in out Test);
+   --  tcl-info.ads:113:4:Commands:Test_Info_Command
+
+   procedure Test_Complete_539361_79b807(Gnattest_T: in out Test);
+   --  tcl-info.ads:136:4:Complete:Test_Info_Complete
+
+   procedure Test_Coroutine_e20165_613b2a(Gnattest_T: in out Test);
+   --  tcl-info.ads:158:4:Coroutine:Test_Info_Coroutine
+
+   procedure Test_Default_c49ada_db99d7(Gnattest_T: in out Test);
+   --  tcl-info.ads:187:4:Default:Test_Info_Default
+
+   procedure Test_ErrorStack_bd77fa_123ed2(Gnattest_T: in out Test);
+   --  tcl-info.ads:209:4:ErrorStack:Test_Info_ErrorStack
+
    procedure Test_Exists_e0ba6a_a2ca41(Gnattest_T: in out Test);
-   --  tcl-info.ads:34:4:Exists:Test_Info_Exists
+   --  tcl-info.ads:230:4:Exists:Test_Info_Exists
+
+   procedure Test_Functions_79eb08_10dc33(Gnattest_T: in out Test);
+   --  tcl-info.ads:256:4:Functions:Test_Info_Functions
+
+   procedure Test_Globals_3e4cb5_c19ad0(Gnattest_T: in out Test);
+   --  tcl-info.ads:282:4:Globals:Test_Info_Globals
+
+   procedure Test_HostName_f98e16_b2a918(Gnattest_T: in out Test);
+   --  tcl-info.ads:304:4:HostName:Test_Info_HostName
+
+   procedure Test_Library_77b287_8b9a20(Gnattest_T: in out Test);
+   --  tcl-info.ads:325:4:Library:Test_Info_Library
+
+   procedure Test_Locals_089928_f4c43e(Gnattest_T: in out Test);
+   --  tcl-info.ads:350:4:Locals:Test_Info_Locals
+
+   procedure Test_Name_Of_Executable_64477f_24ff4c(Gnattest_T: in out Test);
+   --  tcl-info.ads:370:4:Name_Of_Executable:Test_Info_Name_Of_Executable
+
+   procedure Test_Patch_Level_b52e42_325136(Gnattest_T: in out Test);
+   --  tcl-info.ads:391:4:Patch_Level:Test_Info_Patch_Level
+
+   procedure Test_Procs_79f38d_884500(Gnattest_T: in out Test);
+   --  tcl-info.ads:417:4:Procs:Test_Info_Procs
+
+   procedure Test_Script_b67457_ddce00(Gnattest_T: in out Test);
+   --  tcl-info.ads:441:4:Script:Test_Info_Script
+
+   procedure Test_Tcl_Version_f8490f_6661d4(Gnattest_T: in out Test);
+   --  tcl-info.ads:463:4:Tcl_Version:Test_Info_Tcl_Version
+
+   procedure Test_Vars_336d97_9d2a56(Gnattest_T: in out Test);
+   --  tcl-info.ads:488:4:Vars:Test_Info_Vars
 
 end Tcl.Info.Test_Data.Tests;
 --  end read only
