77af34a2aa1e8cad0ee41c0490e6d862b15ee69a
thindil <thindil@laeran.pl>
2021-01-28 06:37:33 +0000

    updated unit tests
    FossilOrigin-Name: fdcb0f1a2ce428cad9f64be3e9e057b73174a26ef9c050ecef4e62ab105a1a13

--- tests/tk-menu-menu_options_test_data-menu_options_tests.ads
+++ tests/tk-menu-menu_options_test_data-menu_options_tests.ads
@@ -12,18 +12,18 @@ package Tk.Menu.Menu_Options_Test_Data.Menu_Options_Tests is
    null record;
 
    procedure Test_Create_32e405_f4d256(Gnattest_T: in out Test_Menu_Options);
-   --  tk-menu.ads:211:4:Create:Test_Create_Menu1
+   --  tk-menu.ads:220:4:Create:Test_Create_Menu1
 
    procedure Test_Create_ebbdc1_ede574(Gnattest_T: in out Test_Menu_Options);
-   --  tk-menu.ads:244:4:Create:Test_Create_Menu2
+   --  tk-menu.ads:253:4:Create:Test_Create_Menu2
 
    procedure Test_Get_Options_ded36e_2834e9
      (Gnattest_T: in out Test_Menu_Options);
-   --  tk-menu.ads:311:4:Get_Options:Test_Get_Options_Menu
+   --  tk-menu.ads:331:4:Get_Options:Test_Get_Options_Menu
 
    procedure Test_Configure_0076be_576a27
      (Gnattest_T: in out Test_Menu_Options);
-   --  tk-menu.ads:357:4:Configure:Test_Configure_Menu
+   --  tk-menu.ads:377:4:Configure:Test_Configure_Menu
 
 end Tk.Menu.Menu_Options_Test_Data.Menu_Options_Tests;
 --  end read only
