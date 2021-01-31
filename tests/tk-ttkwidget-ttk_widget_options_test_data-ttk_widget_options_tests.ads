99581bfcc01c465982b89b1d1dbf8a6ba259d753
thindil <thindil@laeran.pl>
2021-01-31 06:59:25 +0000

    updated unit tests
    FossilOrigin-Name: 9bfbbfecde39410d4654f8e443c908a072b757373e6408bc6bde8e184357406c

---
+++ tests/tk-ttkwidget-ttk_widget_options_test_data-ttk_widget_options_tests.ads
@@ -0,0 +1,18 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with GNATtest_Generated;
+
+package Tk.TtkWidget.Ttk_Widget_Options_Test_Data.Ttk_Widget_Options_Tests is
+
+   type Test_Ttk_Widget_Options is abstract new GNATtest_Generated
+     .GNATtest_Standard
+     .Tk
+     .TtkWidget
+     .Ttk_Widget_Options_Test_Data
+     .Test_Ttk_Widget_Options with
+   null record;
+
+end Tk.TtkWidget.Ttk_Widget_Options_Test_Data.Ttk_Widget_Options_Tests;
+--  end read only
