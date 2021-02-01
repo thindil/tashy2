ea16acede6a18bbc6498ce41c80625c928a04788
thindil <thindil@laeran.pl>
2021-02-01 06:41:36 +0000

    started work on Tk.TtkButton package
    FossilOrigin-Name: f56775f95a1b91950b8454d94d7ca9a7fba7031e48e095c617a1a0eb8b071160

---
+++ src/tk/tk-ttkbutton.ads
@@ -0,0 +1,25 @@
+-- Copyright (c) 2021 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+with Tk.TtkWidget; use Tk.TtkWidget;
+
+package Tk.TtkButton is
+
+   subtype Ttk_Button is Ttk_Widget;
+
+   type Ttk_Button_Options is new Ttk_Widget_Options with null record;
+
+   function Get_Options(Widget: Ttk_Button) return Ttk_Button_Options;
+
+end Tk.TtkButton;
