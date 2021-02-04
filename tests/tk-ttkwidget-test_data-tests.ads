32e814c69820daf75e8cb6a8d8b2eec8a9e7a0ad
thindil <thindil@laeran.pl>
2021-02-04 06:13:45 +0000

    updated unit tests
    FossilOrigin-Name: 3e762cbb37a1ce7e573b4d10033256492c687dee6f5ffe02eaec16b57acdfd85

--- tests/tk-ttkwidget-test_data-tests.ads
+++ tests/tk-ttkwidget-test_data-tests.ads
@@ -10,17 +10,26 @@ package Tk.TtkWidget.Test_Data.Tests is
      .Test with
    null record;
 
+   procedure Test_Option_Image_a3facf_ddcb4f(Gnattest_T: in out Test);
+   --  tk-ttkwidget.ads:146:4:Option_Image:Test_Option_Image_Compound_Type
+
+   procedure Test_Option_Image_5ac754_8ca289(Gnattest_T: in out Test);
+   --  tk-ttkwidget.ads:151:4:Option_Image:Test_Option_Image_Distabled_State_Type
+
+   procedure Test_Option_Image_a0749a_9522a2(Gnattest_T: in out Test);
+   --  tk-ttkwidget.ads:156:4:Option_Image:Test_Option_Image_Image_Option
+
    procedure Test_In_State_46b68b_5aa481(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:103:4:In_State:Test_Ttk_Widget_In_State
+   --  tk-ttkwidget.ads:182:4:In_State:Test_Ttk_Widget_In_State
 
    procedure Test_In_State_318f72_883288(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:128:4:In_State:Test_Ttk_Widget_In_State2
+   --  tk-ttkwidget.ads:207:4:In_State:Test_Ttk_Widget_In_State2
 
    procedure Test_State_291199_c12173(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:152:4:State:Test_Ttk_Widget_State
+   --  tk-ttkwidget.ads:231:4:State:Test_Ttk_Widget_State
 
    procedure Test_State_8d49e9_039b18(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:176:4:State:Test_Ttk_Widget_State2
+   --  tk-ttkwidget.ads:255:4:State:Test_Ttk_Widget_State2
 
 end Tk.TtkWidget.Test_Data.Tests;
 --  end read only
