f46bcac36e4bdafcb1d8dbd331d84d4a60e2135f
thindil <thindil@laeran.pl>
2021-02-07 06:18:21 +0000

    updated unit tests
    FossilOrigin-Name: bcf72aa65a6b1106430284b548bb7555635c6088753acef0ad9fd130db59cef0

--- tests/tcl-lists-test_data-tests.adb
+++ tests/tcl-lists-test_data-tests.adb
@@ -29,7 +29,7 @@ package body Tcl.Lists.Test_Data.Tests is
 --  begin read only
 --  end read only
 --  begin read only
-   function Wrap_Test_Split_List_908c8a_ce6594
+   function Wrap_Test_Split_List_Variable_b25096_53b238
      (Name: String; Interpreter: Tcl_Interpreter := Get_Interpreter)
       return Array_List is
    begin
@@ -40,12 +40,71 @@ package body Tcl.Lists.Test_Data.Tests is
          when System.Assertions.Assert_Failure =>
             AUnit.Assertions.Assert
               (False,
+               "req_sloc(tcl-lists.ads:0):Test_Split_List_Variable test requirement violated");
+      end;
+      declare
+         Test_Split_List_Variable_b25096_53b238_Result: constant Array_List :=
+           GNATtest_Generated.GNATtest_Standard.Tcl.Lists.Split_List_Variable
+             (Name, Interpreter);
+      begin
+         begin
+            pragma Assert(True);
+            null;
+         exception
+            when System.Assertions.Assert_Failure =>
+               AUnit.Assertions.Assert
+                 (False,
+                  "ens_sloc(tcl-lists.ads:0:):Test_Split_List_Variable test commitment violated");
+         end;
+         return Test_Split_List_Variable_b25096_53b238_Result;
+      end;
+   end Wrap_Test_Split_List_Variable_b25096_53b238;
+--  end read only
+
+--  begin read only
+   procedure Test_Split_List_Variable_test_split_list_variable
+     (Gnattest_T: in out Test);
+   procedure Test_Split_List_Variable_b25096_53b238
+     (Gnattest_T: in out Test) renames
+     Test_Split_List_Variable_test_split_list_variable;
+--  id:2.2/b25096b8f19adb69/Split_List_Variable/1/0/test_split_list_variable/
+   procedure Test_Split_List_Variable_test_split_list_variable
+     (Gnattest_T: in out Test) is
+      function Split_List_Variable
+        (Name: String; Interpreter: Tcl_Interpreter := Get_Interpreter)
+         return Array_List renames
+        Wrap_Test_Split_List_Variable_b25096_53b238;
+--  end read only
+
+      pragma Unreferenced(Gnattest_T);
+
+   begin
+
+      AUnit.Assertions.Assert
+        (Gnattest_Generated.Default_Assert_Value, "Test not implemented.");
+
+--  begin read only
+   end Test_Split_List_Variable_test_split_list_variable;
+--  end read only
+
+--  begin read only
+   function Wrap_Test_Split_List_908c8a_b5e33c
+     (List: String; Interpreter: Tcl_Interpreter := Get_Interpreter)
+      return Array_List is
+   begin
+      begin
+         pragma Assert(List'Length > 0 and Interpreter /= Null_Interpreter);
+         null;
+      exception
+         when System.Assertions.Assert_Failure =>
+            AUnit.Assertions.Assert
+              (False,
                "req_sloc(tcl-lists.ads:0):Test_Split_List test requirement violated");
       end;
       declare
-         Test_Split_List_908c8a_ce6594_Result: constant Array_List :=
+         Test_Split_List_908c8a_b5e33c_Result: constant Array_List :=
            GNATtest_Generated.GNATtest_Standard.Tcl.Lists.Split_List
-             (Name, Interpreter);
+             (List, Interpreter);
       begin
          begin
             pragma Assert(True);
@@ -56,21 +115,21 @@ package body Tcl.Lists.Test_Data.Tests is
                  (False,
                   "ens_sloc(tcl-lists.ads:0:):Test_Split_List test commitment violated");
          end;
-         return Test_Split_List_908c8a_ce6594_Result;
+         return Test_Split_List_908c8a_b5e33c_Result;
       end;
-   end Wrap_Test_Split_List_908c8a_ce6594;
+   end Wrap_Test_Split_List_908c8a_b5e33c;
 --  end read only
 
 --  begin read only
    procedure Test_Split_List_test_split_list(Gnattest_T: in out Test);
-   procedure Test_Split_List_908c8a_ce6594(Gnattest_T: in out Test) renames
+   procedure Test_Split_List_908c8a_b5e33c(Gnattest_T: in out Test) renames
      Test_Split_List_test_split_list;
 --  id:2.2/908c8a0cca9c184f/Split_List/1/0/test_split_list/
    procedure Test_Split_List_test_split_list(Gnattest_T: in out Test) is
       function Split_List
-        (Name: String; Interpreter: Tcl_Interpreter := Get_Interpreter)
+        (List: String; Interpreter: Tcl_Interpreter := Get_Interpreter)
          return Array_List renames
-        Wrap_Test_Split_List_908c8a_ce6594;
+        Wrap_Test_Split_List_908c8a_b5e33c;
 --  end read only
 
       pragma Unreferenced(Gnattest_T);
--- tests/tcl-lists-test_data-tests.ads
+++ tests/tcl-lists-test_data-tests.ads
@@ -10,8 +10,11 @@ package Tcl.Lists.Test_Data.Tests is
      .Test with
    null record;
 
-   procedure Test_Split_List_908c8a_ce6594(Gnattest_T: in out Test);
-   --  tcl-lists.ads:47:4:Split_List:Test_Split_List
+   procedure Test_Split_List_Variable_b25096_53b238(Gnattest_T: in out Test);
+   --  tcl-lists.ads:49:4:Split_List_Variable:Test_Split_List_Variable
+
+   procedure Test_Split_List_908c8a_b5e33c(Gnattest_T: in out Test);
+   --  tcl-lists.ads:72:4:Split_List:Test_Split_List
 
 end Tcl.Lists.Test_Data.Tests;
 --  end read only
