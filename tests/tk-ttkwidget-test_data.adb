4c50fbb9910238a12dc545b89cc5100b0700da87
thindil <thindil@laeran.pl>
2021-02-08 06:16:14 +0000

    added destroying Ttk_Widget if needed to tests
    FossilOrigin-Name: 403bada02bcfb89873a4bf90f563efb22d397209ca554a66096a69e50a8b6d86

--- tests/tk-ttkwidget-test_data.adb
+++ tests/tk-ttkwidget-test_data.adb
@@ -3,6 +3,9 @@
 --  automatically. Contents of this package can be modified in any way
 --  except for sections surrounded by a 'read only' marker.
 
+with Ada.Environment_Variables; use Ada.Environment_Variables;
+with Tcl.Info; use Tcl.Info;
+
 package body Tk.TtkWidget.Test_Data is
 
    procedure Set_Up(Gnattest_T: in out Test) is
@@ -13,7 +16,14 @@ package body Tk.TtkWidget.Test_Data is
 
    procedure Tear_Down(Gnattest_T: in out Test) is
       pragma Unreferenced(Gnattest_T);
+      Button: Ttk_Widget;
    begin
-      null;
+      if Value("DISPLAY", "")'Length = 0 then
+         return;
+      end if;
+      Button := Get_Widget(".test");
+      if Button /= Null_Widget then
+         Destroy(Button);
+      end if;
    end Tear_Down;
 end Tk.TtkWidget.Test_Data;
