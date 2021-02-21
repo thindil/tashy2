cedf1220c72de81e2d36cd642113b73c64d668d8
thindil <thindil@laeran.pl>
2021-02-21 06:19:23 +0000

    updated unit tests
    FossilOrigin-Name: 3b5709afb0d03d0c5a003a02d9c998d3194b8bb0d7089972e3dd93471ac4716f

--- tests/tcl-test_data-tests.ads
+++ tests/tcl-test_data-tests.ads
@@ -10,14 +10,14 @@ package Tcl.Test_Data.Tests is
      .Test with
    null record;
 
-   procedure Test_Tcl_Eval_6f41cd_386758(Gnattest_T: in out Test);
-   --  tcl.ads:119:4:Tcl_Eval:Test_Tcl_Eval
+   procedure Test_Tcl_Eval_6f41cd_5b9cd5(Gnattest_T: in out Test);
+   --  tcl.ads:134:4:Tcl_Eval:Test_Tcl_Eval
 
    procedure Test_Tcl_Get_Result_9a7ac3_b83d43(Gnattest_T: in out Test);
-   --  tcl.ads:187:4:Tcl_Get_Result:Test_Tcl_GetResult
+   --  tcl.ads:202:4:Tcl_Get_Result:Test_Tcl_GetResult
 
-   procedure Test_Tcl_Set_Result_2e8975_5a72b9(Gnattest_T: in out Test);
-   --  tcl.ads:248:4:Tcl_Set_Result:Test_Tcl_SetResult
+   procedure Test_Tcl_Set_Result_2e8975_cb8f85(Gnattest_T: in out Test);
+   --  tcl.ads:263:4:Tcl_Set_Result:Test_Tcl_SetResult
 
 end Tcl.Test_Data.Tests;
 --  end read only
