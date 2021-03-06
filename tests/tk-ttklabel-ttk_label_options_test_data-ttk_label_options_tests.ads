34e56eae1da81d0048e03b8d35a37f6e69d05e98
thindil <thindil@laeran.pl>
2021-03-06 06:18:19 +0000

    updated unit tests
    FossilOrigin-Name: 4357fa6b2037d27ef9f91f698c5c1660313ab8fa7f5e1dcdf554925f7cbd1d79

--- tests/tk-ttklabel-ttk_label_options_test_data-ttk_label_options_tests.ads
+++ tests/tk-ttklabel-ttk_label_options_test_data-ttk_label_options_tests.ads
@@ -16,15 +16,15 @@ package Tk.TtkLabel.Ttk_Label_Options_Test_Data.Ttk_Label_Options_Tests is
      (Gnattest_T: in out Test_Ttk_Label_Options);
    --  tk-ttklabel.ads:111:4:Create:Test_Create_TtkLabel1
 
-   procedure Test_Create_ebbdc1_e03243
+   procedure Test_Create_ebbdc1_243d10
      (Gnattest_T: in out Test_Ttk_Label_Options);
    --  tk-ttklabel.ads:146:4:Create:Test_Create_TtkLabel2
 
-   procedure Test_Get_Options_ded36e_97cff9
+   procedure Test_Get_Options_ded36e_b31868
      (Gnattest_T: in out Test_Ttk_Label_Options);
    --  tk-ttklabel.ads:171:4:Get_Options:Test_Get_Options_TtkLabel
 
-   procedure Test_Configure_0076be_d81526
+   procedure Test_Configure_0076be_452ca7
      (Gnattest_T: in out Test_Ttk_Label_Options);
    --  tk-ttklabel.ads:192:4:Configure:Test_Configure_TtkLabel
 
