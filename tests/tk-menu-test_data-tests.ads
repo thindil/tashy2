90406dae859fe8bc8bcf421defc128bfd31ee0d9
thindil <thindil@laeran.pl>
2021-03-11 06:33:00 +0000

    updated unit tests
    FossilOrigin-Name: 8c5bcc46a49c214a2446cb043c023e50d27769f1d623691bfb0300a5b3efa632

--- tests/tk-menu-test_data-tests.ads
+++ tests/tk-menu-test_data-tests.ads
@@ -11,127 +11,127 @@ package Tk.Menu.Test_Data.Tests is
    null record;
 
    procedure Test_Activate_e310e8_54120b(Gnattest_T: in out Test);
-   --  tk-menu.ads:277:4:Activate:Test_Activate_Menu
+   --  tk-menu.ads:306:4:Activate:Test_Activate_Menu
 
    procedure Test_Activate_43ff2b_827a88(Gnattest_T: in out Test);
-   --  tk-menu.ads:280:4:Activate:Test_Activate_Menu2
+   --  tk-menu.ads:309:4:Activate:Test_Activate_Menu2
 
    procedure Test_Activate_c786dd_e56058(Gnattest_T: in out Test);
-   --  tk-menu.ads:285:4:Activate:Test_Activate_Menu3
+   --  tk-menu.ads:314:4:Activate:Test_Activate_Menu3
 
    procedure Test_Add_ba4759_5ef6e5(Gnattest_T: in out Test);
-   --  tk-menu.ads:308:4:Add:Test_Add_Menu
+   --  tk-menu.ads:337:4:Add:Test_Add_Menu
 
    procedure Test_Clone_cce6c8_8fed8c(Gnattest_T: in out Test);
-   --  tk-menu.ads:355:4:Clone:Test_Clone_Menu
+   --  tk-menu.ads:384:4:Clone:Test_Clone_Menu
 
    procedure Test_Delete_a9ab1b_9992c1(Gnattest_T: in out Test);
-   --  tk-menu.ads:403:4:Delete:Test_Delete_Menu
+   --  tk-menu.ads:439:4:Delete:Test_Delete_Menu
 
    procedure Test_Delete_6e86ea_8b9fc5(Gnattest_T: in out Test);
-   --  tk-menu.ads:408:4:Delete:Test_Delete_Menu2
+   --  tk-menu.ads:444:4:Delete:Test_Delete_Menu2
 
    procedure Test_Delete_99fa1b_da6433(Gnattest_T: in out Test);
-   --  tk-menu.ads:413:4:Delete:Test_Delete_Menu3
+   --  tk-menu.ads:449:4:Delete:Test_Delete_Menu3
 
    procedure Test_Entry_Get_Options_ef78cc_729271(Gnattest_T: in out Test);
-   --  tk-menu.ads:441:4:Entry_Get_Options:Test_Entry_Get_Options_Menu
+   --  tk-menu.ads:477:4:Entry_Get_Options:Test_Entry_Get_Options_Menu
 
    procedure Test_Entry_Get_Options_0615ce_9f990e(Gnattest_T: in out Test);
-   --  tk-menu.ads:446:4:Entry_Get_Options:Test_Entry_Get_Options_Menu2
+   --  tk-menu.ads:482:4:Entry_Get_Options:Test_Entry_Get_Options_Menu2
 
    procedure Test_Entry_Get_Options_d1b552_f00eb7(Gnattest_T: in out Test);
-   --  tk-menu.ads:451:4:Entry_Get_Options:Test_Entry_Get_Options_Menu3
+   --  tk-menu.ads:487:4:Entry_Get_Options:Test_Entry_Get_Options_Menu3
 
    procedure Test_Entry_Configure_1d51e9_fc4fb1(Gnattest_T: in out Test);
-   --  tk-menu.ads:477:4:Entry_Configure:Test_Entry_Configure_Menu
+   --  tk-menu.ads:513:4:Entry_Configure:Test_Entry_Configure_Menu
 
    procedure Test_Entry_Configure_73d2b1_c3ebe5(Gnattest_T: in out Test);
-   --  tk-menu.ads:482:4:Entry_Configure:Test_Entry_Configure_Menu2
+   --  tk-menu.ads:518:4:Entry_Configure:Test_Entry_Configure_Menu2
 
    procedure Test_Entry_Configure_138c3b_1d928b(Gnattest_T: in out Test);
-   --  tk-menu.ads:487:4:Entry_Configure:Test_Entry_Configure_Menu3
+   --  tk-menu.ads:523:4:Entry_Configure:Test_Entry_Configure_Menu3
 
    procedure Test_Index_d34072_0f3076(Gnattest_T: in out Test);
-   --  tk-menu.ads:513:4:Index:Test_Index_Menu
+   --  tk-menu.ads:549:4:Index:Test_Index_Menu
 
    procedure Test_Index_032cb4_ad9207(Gnattest_T: in out Test);
-   --  tk-menu.ads:518:4:Index:Test_Index_Menu2
+   --  tk-menu.ads:554:4:Index:Test_Index_Menu2
 
    procedure Test_Index_e3e5f0_c1d386(Gnattest_T: in out Test);
-   --  tk-menu.ads:522:4:Index:Test_Index_Menu3
+   --  tk-menu.ads:558:4:Index:Test_Index_Menu3
 
    procedure Test_Insert_3b526a_33fb34(Gnattest_T: in out Test);
-   --  tk-menu.ads:551:4:Insert:Test_Insert_Menu
+   --  tk-menu.ads:587:4:Insert:Test_Insert_Menu
 
    procedure Test_Insert_fde4b6_24d8d2(Gnattest_T: in out Test);
-   --  tk-menu.ads:556:4:Insert:Test_Insert_Menu2
+   --  tk-menu.ads:592:4:Insert:Test_Insert_Menu2
 
    procedure Test_Insert_3d057c_d006b1(Gnattest_T: in out Test);
-   --  tk-menu.ads:561:4:Insert:Test_Insert_Menu3
+   --  tk-menu.ads:597:4:Insert:Test_Insert_Menu3
 
    procedure Test_Invoke_49d5a3_7b2d3d(Gnattest_T: in out Test);
-   --  tk-menu.ads:587:4:Invoke:Test_Invoke_Menu1
+   --  tk-menu.ads:623:4:Invoke:Test_Invoke_Menu1
 
    procedure Test_Invoke_7593eb_d8311f(Gnattest_T: in out Test);
-   --  tk-menu.ads:590:4:Invoke:Test_Invoke_Menu3
+   --  tk-menu.ads:626:4:Invoke:Test_Invoke_Menu3
 
    procedure Test_Invoke_d16f08_c23171(Gnattest_T: in out Test);
-   --  tk-menu.ads:595:4:Invoke:Test_Invoke_Menu4
+   --  tk-menu.ads:631:4:Invoke:Test_Invoke_Menu4
 
    procedure Test_Invoke_0a3e65_d5e898(Gnattest_T: in out Test);
-   --  tk-menu.ads:621:4:Invoke:Test_Invoke_Menu2
+   --  tk-menu.ads:657:4:Invoke:Test_Invoke_Menu2
 
    procedure Test_Invoke_781f8e_c01adc(Gnattest_T: in out Test);
-   --  tk-menu.ads:625:4:Invoke:Test_Invoke_Menu5
+   --  tk-menu.ads:661:4:Invoke:Test_Invoke_Menu5
 
    procedure Test_Invoke_a338ab_66d08a(Gnattest_T: in out Test);
-   --  tk-menu.ads:630:4:Invoke:Test_Invoke_Menu6
+   --  tk-menu.ads:666:4:Invoke:Test_Invoke_Menu6
 
    procedure Test_Post_297225_baf848(Gnattest_T: in out Test);
-   --  tk-menu.ads:655:4:Post:Test_Post_Menu1
+   --  tk-menu.ads:691:4:Post:Test_Post_Menu1
 
    procedure Test_Post_311578_f13317(Gnattest_T: in out Test);
-   --  tk-menu.ads:685:4:Post:Test_Post_Menu2
+   --  tk-menu.ads:721:4:Post:Test_Post_Menu2
 
    procedure Test_Post_Cascade_09822b_aeea07(Gnattest_T: in out Test);
-   --  tk-menu.ads:707:4:Post_Cascade:Test_PostCascade_Menu
+   --  tk-menu.ads:743:4:Post_Cascade:Test_PostCascade_Menu
 
    procedure Test_Post_Cascade_cc0d55_c83524(Gnattest_T: in out Test);
-   --  tk-menu.ads:710:4:Post_Cascade:Test_PostCascade_Menu2
+   --  tk-menu.ads:746:4:Post_Cascade:Test_PostCascade_Menu2
 
    procedure Test_Post_Cascade_38af3a_1e3e67(Gnattest_T: in out Test);
-   --  tk-menu.ads:715:4:Post_Cascade:Test_PostCascade_Menu3
+   --  tk-menu.ads:751:4:Post_Cascade:Test_PostCascade_Menu3
 
    procedure Test_Get_Item_Type_a8752e_8c54de(Gnattest_T: in out Test);
-   --  tk-menu.ads:737:4:Get_Item_Type:Test_Get_Item_Type_Menu
+   --  tk-menu.ads:773:4:Get_Item_Type:Test_Get_Item_Type_Menu
 
    procedure Test_Get_Item_Type_2b168c_962e70(Gnattest_T: in out Test);
-   --  tk-menu.ads:741:4:Get_Item_Type:Test_Get_Item_Type_Menu2
+   --  tk-menu.ads:777:4:Get_Item_Type:Test_Get_Item_Type_Menu2
 
    procedure Test_Get_Item_Type_8d689d_527e33(Gnattest_T: in out Test);
-   --  tk-menu.ads:746:4:Get_Item_Type:Test_Get_Item_Type_Menu3
+   --  tk-menu.ads:782:4:Get_Item_Type:Test_Get_Item_Type_Menu3
 
    procedure Test_Unpost_74fd1c_cf7a1c(Gnattest_T: in out Test);
-   --  tk-menu.ads:766:4:Unpost:Test_Unpost_Menu
+   --  tk-menu.ads:802:4:Unpost:Test_Unpost_Menu
 
    procedure Test_X_Position_2f4d2c_e9e27f(Gnattest_T: in out Test);
-   --  tk-menu.ads:791:4:X_Position:Test_X_Position_Menu
+   --  tk-menu.ads:827:4:X_Position:Test_X_Position_Menu
 
    procedure Test_X_Position_789329_f6fff3(Gnattest_T: in out Test);
-   --  tk-menu.ads:795:4:X_Position:Test_X_Position_Menu2
+   --  tk-menu.ads:831:4:X_Position:Test_X_Position_Menu2
 
    procedure Test_X_Position_a37ded_0ad35d(Gnattest_T: in out Test);
-   --  tk-menu.ads:800:4:X_Position:Test_X_Position_Menu3
+   --  tk-menu.ads:836:4:X_Position:Test_X_Position_Menu3
 
    procedure Test_Y_Position_623b60_3f1329(Gnattest_T: in out Test);
-   --  tk-menu.ads:826:4:Y_Position:Test_Y_Position_Menu
+   --  tk-menu.ads:862:4:Y_Position:Test_Y_Position_Menu
 
    procedure Test_Y_Position_7e1bbe_2e80d9(Gnattest_T: in out Test);
-   --  tk-menu.ads:830:4:Y_Position:Test_Y_Position_Menu2
+   --  tk-menu.ads:866:4:Y_Position:Test_Y_Position_Menu2
 
    procedure Test_Y_Position_9d49dc_1c0642(Gnattest_T: in out Test);
-   --  tk-menu.ads:835:4:Y_Position:Test_Y_Position_Menu3
+   --  tk-menu.ads:871:4:Y_Position:Test_Y_Position_Menu3
 
 end Tk.Menu.Test_Data.Tests;
 --  end read only
