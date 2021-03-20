0fd2691819da06ceda943700a7e892a9d3443ac5
thindil <thindil@laeran.pl>
2021-03-20 05:47:15 +0000

    fixed some AdaControl errors
    FossilOrigin-Name: 718fb42f2a9293547f4473a4f49a6a96125ee0c9de9837773dd72bd2b7bfd57d

--- src/tk/tk-ttkwidget.adb
+++ src/tk/tk-ttkwidget.adb
@@ -154,52 +154,65 @@ package body Tk.TtkWidget is
    function Option_Value
      (Ttk_Widgt: Ttk_Widget; Name: String) return Disabled_State_Type is
    begin
-      Execute_Widget_Command(Ttk_Widgt, "cget", "-" & Name);
-      return Disabled_State_Type'Value(Tcl_Get_Result(Tk_Interp(Ttk_Widgt)));
+      Execute_Widget_Command
+        (Widgt => Ttk_Widgt, Command_Name => "cget", Options => "-" & Name);
+      return Disabled_State_Type'Value
+          (Tcl_Get_Result(Interpreter => Tk_Interp(Widgt => Ttk_Widgt)));
    end Option_Value;
 
    function Option_Value
      (Ttk_Widgt: Ttk_Widget; Name: String) return Ttk_Image_Option is
       Options: Ttk_Image_Option := Ttk_Image_Option'(others => <>);
    begin
-      Execute_Widget_Command(Ttk_Widgt, "cget", "-" & Name);
+      Execute_Widget_Command
+        (Widgt => Ttk_Widgt, Command_Name => "cget", Options => "-" & Name);
+      Get_Options_Block :
       declare
          Options_Array: constant Array_List :=
-           Split_List(Tcl_Get_Result(Tk_Interp(Ttk_Widgt)));
+           Split_List
+             (List =>
+                Tcl_Get_Result(Interpreter => Tk_Interp(Widgt => Ttk_Widgt)),
+              Interpreter => Tk_Interp(Widgt => Ttk_Widgt));
          Index: Positive := 2;
       begin
          if Options_Array'Length < 1 then
             return Options;
          end if;
          Options.Default :=
-           To_Tcl_String(To_String(Source => Options_Array(1)));
+           To_Tcl_String(Source => To_String(Source => Options_Array(1)));
          Set_Options_Loop :
-         loop
-            exit Set_Options_Loop when Index > Options_Array'Length;
-            if Options_Array(Index) = To_Tcl_String("active") then
+         while Index <= Options_Array'Length loop
+            if Options_Array(Index) = To_Tcl_String(Source => "active") then
                Options.Active := Options_Array(Index + 1);
-            elsif Options_Array(Index) = To_Tcl_String("disabled") then
+            elsif Options_Array(Index) =
+              To_Tcl_String(Source => "disabled") then
                Options.Disabled := Options_Array(Index + 1);
-            elsif Options_Array(Index) = To_Tcl_String("focus") then
+            elsif Options_Array(Index) = To_Tcl_String(Source => "focus") then
                Options.Focus := Options_Array(Index + 1);
-            elsif Options_Array(Index) = To_Tcl_String("pressed") then
+            elsif Options_Array(Index) =
+              To_Tcl_String(Source => "pressed") then
                Options.Pressed := Options_Array(Index + 1);
-            elsif Options_Array(Index) = To_Tcl_String("selected") then
+            elsif Options_Array(Index) =
+              To_Tcl_String(Source => "selected") then
                Options.Selected := Options_Array(Index + 1);
-            elsif Options_Array(Index) = To_Tcl_String("background") then
+            elsif Options_Array(Index) =
+              To_Tcl_String(Source => "background") then
                Options.Background := Options_Array(Index + 1);
-            elsif Options_Array(Index) = To_Tcl_String("readonly") then
+            elsif Options_Array(Index) =
+              To_Tcl_String(Source => "readonly") then
                Options.Readonly := Options_Array(Index + 1);
-            elsif Options_Array(Index) = To_Tcl_String("alternate") then
+            elsif Options_Array(Index) =
+              To_Tcl_String(Source => "alternate") then
                Options.Alternate := Options_Array(Index + 1);
-            elsif Options_Array(Index) = To_Tcl_String("invalid") then
+            elsif Options_Array(Index) =
+              To_Tcl_String(Source => "invalid") then
                Options.Invalid := Options_Array(Index + 1);
-            elsif Options_Array(Index) = To_Tcl_String("hover") then
+            elsif Options_Array(Index) = To_Tcl_String(Source => "hover") then
                Options.Hover := Options_Array(Index + 1);
             end if;
             Index := Index + 2;
          end loop Set_Options_Loop;
-      end;
+      end Get_Options_Block;
       return Options;
    end Option_Value;
 
