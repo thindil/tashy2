9c83cb046c7124e94f023c01d46d7cbfc26b398e
thindil <thindil@laeran.pl>
2021-03-29 04:54:27 +0000

    updated unit tests
    FossilOrigin-Name: c87d9c4707f41500de2d3435a5a89ac0e27135bf8db5f95810dd802a8241af26

--- tests/tk-grid-test_data-tests.ads
+++ tests/tk-grid-test_data-tests.ads
@@ -23,67 +23,67 @@ package Tk.Grid.Test_Data.Tests is
    --  tk-grid.ads:200:4:Anchor:Test_Anchor2
 
    procedure Test_Bounding_Box_251762_896807(Gnattest_T: in out Test);
-   --  tk-grid.ads:237:4:Bounding_Box:Test_BBox
+   --  tk-grid.ads:241:4:Bounding_Box:Test_BBox
 
    procedure Test_Column_Configure_b41aca_dcc0a7(Gnattest_T: in out Test);
-   --  tk-grid.ads:262:4:Column_Configure:Test_Column_Configure1
+   --  tk-grid.ads:266:4:Column_Configure:Test_Column_Configure1
 
    procedure Test_Column_Configure_dbcaa4_baf62d(Gnattest_T: in out Test);
-   --  tk-grid.ads:287:4:Column_Configure:Test_Column_Configure2
+   --  tk-grid.ads:291:4:Column_Configure:Test_Column_Configure2
 
    procedure Test_Column_Configure_86279e_3e25dc(Gnattest_T: in out Test);
-   --  tk-grid.ads:312:4:Column_Configure:Test_Column_Configure3
+   --  tk-grid.ads:316:4:Column_Configure:Test_Column_Configure3
 
    procedure Test_Get_Column_Options_22be63_f54751(Gnattest_T: in out Test);
-   --  tk-grid.ads:336:4:Get_Column_Options:Test_Get_Column_Options
+   --  tk-grid.ads:339:4:Get_Column_Options:Test_Get_Column_Options
 
    procedure Test_Configure_c67185_4765c0(Gnattest_T: in out Test);
-   --  tk-grid.ads:358:4:Configure:Test_Configure1
+   --  tk-grid.ads:361:4:Configure:Test_Configure1
 
    procedure Test_Configure_f31b66_3e2feb(Gnattest_T: in out Test);
-   --  tk-grid.ads:379:4:Configure:Test_Configure2
+   --  tk-grid.ads:382:4:Configure:Test_Configure2
 
    procedure Test_Forget_7f8bc2_8fa399(Gnattest_T: in out Test);
-   --  tk-grid.ads:401:4:Forget:Test_Forget1
+   --  tk-grid.ads:404:4:Forget:Test_Forget1
 
    procedure Test_Forget_41161d_27199d(Gnattest_T: in out Test);
-   --  tk-grid.ads:423:4:Forget:Test_Forget2
+   --  tk-grid.ads:426:4:Forget:Test_Forget2
 
    procedure Test_Info_5c8f4b_ba34a0(Gnattest_T: in out Test);
-   --  tk-grid.ads:444:4:Info:Test_Info
+   --  tk-grid.ads:447:4:Info:Test_Info
 
    procedure Test_Location_01f15c_74cec4(Gnattest_T: in out Test);
-   --  tk-grid.ads:475:4:Location:Test_Location
+   --  tk-grid.ads:478:4:Location:Test_Location
 
    procedure Test_Propagate_eff519_e08f8c(Gnattest_T: in out Test);
-   --  tk-grid.ads:497:4:Propagate:Test_Propagate1
+   --  tk-grid.ads:500:4:Propagate:Test_Propagate1
 
    procedure Test_Propagate_306472_142129(Gnattest_T: in out Test);
-   --  tk-grid.ads:520:4:Propagate:Test_Propagate2
+   --  tk-grid.ads:523:4:Propagate:Test_Propagate2
 
    procedure Test_Row_Configure_cd10c1_55f5f1(Gnattest_T: in out Test);
-   --  tk-grid.ads:543:4:Row_Configure:Test_Row_Configure1
+   --  tk-grid.ads:549:4:Row_Configure:Test_Row_Configure1
 
    procedure Test_Row_Configure_0a2e60_353acd(Gnattest_T: in out Test);
-   --  tk-grid.ads:568:4:Row_Configure:Test_Row_Configure2
+   --  tk-grid.ads:574:4:Row_Configure:Test_Row_Configure2
 
    procedure Test_Row_Configure_7261aa_f7627e(Gnattest_T: in out Test);
-   --  tk-grid.ads:593:4:Row_Configure:Test_Row_Configure3
+   --  tk-grid.ads:599:4:Row_Configure:Test_Row_Configure3
 
    procedure Test_Get_Row_Options_192829_8edf28(Gnattest_T: in out Test);
-   --  tk-grid.ads:617:4:Get_Row_Options:Test_Get_Row_Options
+   --  tk-grid.ads:622:4:Get_Row_Options:Test_Get_Row_Options
 
    procedure Test_Remove_cf1782_4cc907(Gnattest_T: in out Test);
-   --  tk-grid.ads:642:4:Remove:Test_Remove1
+   --  tk-grid.ads:647:4:Remove:Test_Remove1
 
    procedure Test_Remove_4f07d4_13d0b1(Gnattest_T: in out Test);
-   --  tk-grid.ads:666:4:Remove:Test_Remove2
+   --  tk-grid.ads:671:4:Remove:Test_Remove2
 
    procedure Test_Size_ec983b_a60758(Gnattest_T: in out Test);
-   --  tk-grid.ads:687:4:Size:Test_Size
+   --  tk-grid.ads:692:4:Size:Test_Size
 
    procedure Test_Slaves_3ea16d_8c1fbf(Gnattest_T: in out Test);
-   --  tk-grid.ads:711:4:Slaves:Test_Slaves
+   --  tk-grid.ads:716:4:Slaves:Test_Slaves
 
 end Tk.Grid.Test_Data.Tests;
 --  end read only
