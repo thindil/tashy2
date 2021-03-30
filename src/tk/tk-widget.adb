fa100e5c818bfdd858108c05629d4159e93bfa66
thindil <thindil@laeran.pl>
2021-03-30 05:26:47 +0000

    added generic functions for execute Tk widgets commands
    FossilOrigin-Name: f8941230a63a62e23459488f0d831155e7cb78a78f660828da50a7285195d15b

--- src/tk/tk-widget.adb
+++ src/tk/tk-widget.adb
@@ -467,4 +467,32 @@ package body Tk.Widget is
            Interpreter => Tk_Interp(Widgt => Widgt));
    end Execute_Widget_Command;
 
+   function Generic_Scalar_Execute_Widget_Command
+     (Widgt: Tk_Widget; Command_Name: String; Options: String := "")
+      return Result_Type is
+   begin
+      if Widgt = Null_Widget then
+         raise Tcl_Exception
+           with "Can't get result from non existing Tk widget";
+      end if;
+      return Result_Type'Value
+          (Execute_Widget_Command
+             (Widgt => Widgt, Command_Name => Command_Name,
+              Options => Options));
+   end Generic_Scalar_Execute_Widget_Command;
+
+   function Generic_Float_Execute_Widget_Command
+     (Widgt: Tk_Widget; Command_Name: String; Options: String := "")
+      return Result_Type is
+   begin
+      if Widgt = Null_Widget then
+         raise Tcl_Exception
+           with "Can't get result from non existing Tk widget";
+      end if;
+      return Result_Type'Value
+          (Execute_Widget_Command
+             (Widgt => Widgt, Command_Name => Command_Name,
+              Options => Options));
+   end Generic_Float_Execute_Widget_Command;
+
 end Tk.Widget;
