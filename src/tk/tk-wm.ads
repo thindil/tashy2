e242738fb511de675dd2ca54ff0a5d3a0ee7b7e5
thindil <thindil@laeran.pl>
2021-04-01 05:57:10 +0000

    continue work on binding for Tk wm commands
    FossilOrigin-Name: e77b1a4be81c8bfe5b20b3bb60dd9a0a604ad73ec070d315323869f9b0dbbf0e

--- src/tk/tk-wm.ads
+++ src/tk/tk-wm.ads
@@ -12,6 +12,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+with Tcl.Strings; use Tcl.Strings;
 with Tk.Widget; use Tk.Widget;
 
 package Tk.Wm is
@@ -20,18 +21,70 @@ package Tk.Wm is
 
    type Alpha_Type is digits 2 range -1.0 .. 1.0;
 
-   type Window_Attributes_Data (WM_Type: Window_Manager_Types := X11) is record
+   type Window_Types is
+     (DESKTOP, DOCK, TOOLBAR, MENU, UTILITY, SPLASH, DIALOG, DROPDOWN_MENU,
+      POPUP_MENU, TOOLTIP, NOTIFICATION, COMBO, DND, NORMAL);
+
+   type Window_Attributes_Data(WM_Type: Window_Manager_Types := X11) is record
       Alpha: Alpha_Type;
+      Full_Screen: Extended_Boolean;
+      Topmost: Extended_Boolean;
+      case WM_Type is
+         when X11 =>
+            Window_Type: Window_Types;
+            Zoomed: Extended_Boolean;
+         when WINDOWS =>
+            Disabled: Extended_Boolean;
+            Tool_Window: Extended_Boolean;
+            Transparent_Color: Tcl_String;
+         when MACOSX =>
+            Modified: Extended_Boolean;
+            Notify: Extended_Boolean;
+            Title_Path: Tcl_String;
+            Transparent: Extended_Boolean;
+      end case;
    end record;
 
    procedure Aspect
      (Window: Tk_Widget;
-      Min_Numer, Min_Denom, Max_Numer, Max_Denom: Extended_Natural) with
+      Min_Numer, Min_Denom, Max_Numer, Max_Denom: Natural) with
       Pre => Window /= Null_Widget,
       Test_Case => (Name => "Test_Wm_Aspect", Mode => Nominal);
 
    function Aspect(Window: Tk_Widget) return Bbox_Array with
       Pre => Window /= Null_Widget,
       Test_Case => (Name => "Test_Wm_Aspect2", Mode => Nominal);
+
+   function Get_Attributes(Window: Tk_Widget) return Window_Attributes_Data with
+      Pre => Window /= Null_Widget,
+      Test_Case => (Name => "Test_Wm_Get_Attributes", Mode => Nominal);
+
+   procedure Set_Attributes(Window: Tk_Widget; Attributes_Data: Window_Attributes_Data) with
+      Pre => Window /= Null_Widget,
+      Test_Case => (Name => "Test_Wm_Set_Attributes", Mode => Nominal);
+
+   function Get_Attribute(Window: Tk_Widget; Name: String) return Tcl_String with
+      Pre => Window /= Null_Widget and Name'Length > 0,
+      Test_Case => (Name => "Test_Wm_Get_Attribute", Mode => Nominal);
+
+   function Get_Attribute(Window: Tk_Widget; Name: String) return Extended_Boolean with
+      Pre => Window /= Null_Widget and Name'Length > 0,
+      Test_Case => (Name => "Test_Wm_Get_Attribute2", Mode => Nominal);
+
+   function Get_Attribute(Window: Tk_Widget; Name: String) return Alpha_Type with
+      Pre => Window /= Null_Widget and Name'Length > 0,
+      Test_Case => (Name => "Test_Wm_Get_Attribute3", Mode => Nominal);
+
+   function Get_Attribute(Window: Tk_Widget; Name: String) return Window_Types with
+      Pre => Window /= Null_Widget and Name'Length > 0,
+      Test_Case => (Name => "Test_Wm_Get_Attribute4", Mode => Nominal);
+
+   function Client(Window: Tk_Widget) return String with
+      Pre => Window /= Null_Widget,
+      Test_Case => (Name => "Test_Wm_Client", Mode => Nominal);
+
+   procedure Client(Window: Tk_Widget; Name: Tcl_String) with
+      Pre => Window /= Null_Widget and Length(Name) > 0,
+      Test_Case => (Name => "Test_Wm_Client2", Mode => Nominal);
 
 end Tk.Wm;
