407c1050fd4c8f3b61b853f2ad4917f445c9ab4a
thindil <thindil@laeran.pl>
2021-04-08 05:46:03 +0000

    updated unit tests
    FossilOrigin-Name: f9bafc48279334379e5f687b220af0e80be420e0c9bea160451d8a3e00f07fe0

--- tests/tk-wm-test_data-tests.ads
+++ tests/tk-wm-test_data-tests.ads
@@ -11,115 +11,115 @@ package Tk.Wm.Test_Data.Tests is
    null record;
 
    procedure Test_Aspect_a5b4d0_4b5997(Gnattest_T: in out Test);
-   --  tk-wm.ads:97:4:Aspect:Test_Wm_Aspect
+   --  tk-wm.ads:98:4:Aspect:Test_Wm_Aspect
 
    procedure Test_Aspect_d213ed_d6b574(Gnattest_T: in out Test);
-   --  tk-wm.ads:103:4:Aspect:Test_Wm_Aspect2
+   --  tk-wm.ads:104:4:Aspect:Test_Wm_Aspect2
 
    procedure Test_Get_Attributes_0c5f59_7e1726(Gnattest_T: in out Test);
-   --  tk-wm.ads:107:4:Get_Attributes:Test_Wm_Get_Attributes
+   --  tk-wm.ads:108:4:Get_Attributes:Test_Wm_Get_Attributes
 
    procedure Test_Set_Attributes_58cf2d_d28502(Gnattest_T: in out Test);
-   --  tk-wm.ads:112:4:Set_Attributes:Test_Wm_Set_Attributes
+   --  tk-wm.ads:113:4:Set_Attributes:Test_Wm_Set_Attributes
 
    procedure Test_Get_Attribute_278e4a_747c75(Gnattest_T: in out Test);
-   --  tk-wm.ads:117:4:Get_Attribute:Test_Wm_Get_Attribute
+   --  tk-wm.ads:118:4:Get_Attribute:Test_Wm_Get_Attribute
 
    procedure Test_Get_Attribute_c2eb34_2dc50d(Gnattest_T: in out Test);
-   --  tk-wm.ads:122:4:Get_Attribute:Test_Wm_Get_Attribute2
+   --  tk-wm.ads:123:4:Get_Attribute:Test_Wm_Get_Attribute2
 
    procedure Test_Get_Attribute_bcffd5_2122fe(Gnattest_T: in out Test);
-   --  tk-wm.ads:127:4:Get_Attribute:Test_Wm_Get_Attribute3
+   --  tk-wm.ads:128:4:Get_Attribute:Test_Wm_Get_Attribute3
 
    procedure Test_Get_Attribute_731b81_56b1b4(Gnattest_T: in out Test);
-   --  tk-wm.ads:132:4:Get_Attribute:Test_Wm_Get_Attribute4
+   --  tk-wm.ads:133:4:Get_Attribute:Test_Wm_Get_Attribute4
 
    procedure Test_Client_fda74e_991856(Gnattest_T: in out Test);
-   --  tk-wm.ads:137:4:Client:Test_Wm_Client
+   --  tk-wm.ads:138:4:Client:Test_Wm_Client
 
    procedure Test_Client_563118_ac4719(Gnattest_T: in out Test);
-   --  tk-wm.ads:141:4:Client:Test_Wm_Client2
+   --  tk-wm.ads:142:4:Client:Test_Wm_Client2
 
    procedure Test_Color_Map_Windows_4e073e_798c45(Gnattest_T: in out Test);
-   --  tk-wm.ads:145:4:Color_Map_Windows:Test_Wm_Color_Map_Windows
+   --  tk-wm.ads:146:4:Color_Map_Windows:Test_Wm_Color_Map_Windows
 
    procedure Test_Color_Map_Windows_81bc98_d4205a(Gnattest_T: in out Test);
-   --  tk-wm.ads:149:4:Color_Map_Windows:Test_Wm_Color_Map_Windows2
+   --  tk-wm.ads:150:4:Color_Map_Windows:Test_Wm_Color_Map_Windows2
 
    procedure Test_Command_769e81_cb0619(Gnattest_T: in out Test);
-   --  tk-wm.ads:153:4:Command:Test_Wm_Command
+   --  tk-wm.ads:154:4:Command:Test_Wm_Command
 
    procedure Test_Command_7808c4_2c724c(Gnattest_T: in out Test);
-   --  tk-wm.ads:157:4:Command:Test_Wm_Command2
+   --  tk-wm.ads:158:4:Command:Test_Wm_Command2
 
    procedure Test_Deiconify_46ab19_2f30b1(Gnattest_T: in out Test);
-   --  tk-wm.ads:161:4:Deiconify:Test_Wm_Deiconify
+   --  tk-wm.ads:162:4:Deiconify:Test_Wm_Deiconify
 
    procedure Test_Focus_Model_e88243_857661(Gnattest_T: in out Test);
-   --  tk-wm.ads:165:4:Focus_Model:Test_Wm_Focus_Model
+   --  tk-wm.ads:166:4:Focus_Model:Test_Wm_Focus_Model
 
    procedure Test_Focus_Model_93cac7_9f50c8(Gnattest_T: in out Test);
-   --  tk-wm.ads:169:4:Focus_Model:Test_Wm_Focus_Model2
+   --  tk-wm.ads:170:4:Focus_Model:Test_Wm_Focus_Model2
 
    procedure Test_Forget_7f8bc2_846bd1(Gnattest_T: in out Test);
-   --  tk-wm.ads:173:4:Forget:Test_Wm_Forget
+   --  tk-wm.ads:174:4:Forget:Test_Wm_Forget
 
    procedure Test_Frame_5d04b9_b86ec4(Gnattest_T: in out Test);
-   --  tk-wm.ads:177:4:Frame:Test_Wm_Frame
+   --  tk-wm.ads:178:4:Frame:Test_Wm_Frame
 
    procedure Test_Geometry_ba948b_4f4f37(Gnattest_T: in out Test);
-   --  tk-wm.ads:181:4:Geometry:Test_Wm_Geometry
+   --  tk-wm.ads:182:4:Geometry:Test_Wm_Geometry
 
    procedure Test_Geometry_d2a7fd_9bd66a(Gnattest_T: in out Test);
-   --  tk-wm.ads:185:4:Geometry:Test_Wm_Geometry2
+   --  tk-wm.ads:186:4:Geometry:Test_Wm_Geometry2
 
    procedure Test_Grid_200f57_fc763d(Gnattest_T: in out Test);
-   --  tk-wm.ads:190:4:Grid:Test_Wm_Grid
+   --  tk-wm.ads:191:4:Grid:Test_Wm_Grid
 
    procedure Test_Grid_504629_1fe5bc(Gnattest_T: in out Test);
-   --  tk-wm.ads:194:4:Grid:Test_Wm_Grid2
+   --  tk-wm.ads:195:4:Grid:Test_Wm_Grid2
 
    procedure Test_Group_928167_42ff38(Gnattest_T: in out Test);
-   --  tk-wm.ads:201:4:Group:Test_Wm_Group
+   --  tk-wm.ads:202:4:Group:Test_Wm_Group
 
    procedure Test_Group_fe3a0f_95a2e7(Gnattest_T: in out Test);
-   --  tk-wm.ads:205:4:Group:Test_Wm_Group2
+   --  tk-wm.ads:206:4:Group:Test_Wm_Group2
 
    procedure Test_Icon_Bitmap_51d509_127eea(Gnattest_T: in out Test);
-   --  tk-wm.ads:209:4:Icon_Bitmap:Test_Wm_Icon_Bitmap
+   --  tk-wm.ads:210:4:Icon_Bitmap:Test_Wm_Icon_Bitmap
 
    procedure Test_Icon_Bitmap_93e489_31ebac(Gnattest_T: in out Test);
-   --  tk-wm.ads:213:4:Icon_Bitmap:Test_Wm_Icon_Bitmap2
+   --  tk-wm.ads:214:4:Icon_Bitmap:Test_Wm_Icon_Bitmap2
 
    procedure Test_Iconify_b7aad2_844804(Gnattest_T: in out Test);
-   --  tk-wm.ads:217:4:Iconify:Test_Wm_Iconify
+   --  tk-wm.ads:218:4:Iconify:Test_Wm_Iconify
 
    procedure Test_Icon_Mask_001121_818a6a(Gnattest_T: in out Test);
-   --  tk-wm.ads:221:4:Icon_Mask:Test_Wm_Icon_Mask
+   --  tk-wm.ads:222:4:Icon_Mask:Test_Wm_Icon_Mask
 
    procedure Test_Icon_Mask_66e29a_b2153c(Gnattest_T: in out Test);
-   --  tk-wm.ads:225:4:Icon_Mask:Test_Wm_Icon_Mask2
+   --  tk-wm.ads:226:4:Icon_Mask:Test_Wm_Icon_Mask2
 
    procedure Test_Icon_Name_4b1072_55536b(Gnattest_T: in out Test);
-   --  tk-wm.ads:229:4:Icon_Name:Test_Wm_Icon_Name
+   --  tk-wm.ads:230:4:Icon_Name:Test_Wm_Icon_Name
 
    procedure Test_Icon_Name_5bfbf7_3f7273(Gnattest_T: in out Test);
-   --  tk-wm.ads:233:4:Icon_Name:Test_Wm_Icon_Name2
+   --  tk-wm.ads:234:4:Icon_Name:Test_Wm_Icon_Name2
 
    procedure Test_Icon_Photo_69d087_a0feb6(Gnattest_T: in out Test);
-   --  tk-wm.ads:237:4:Icon_Photo:Test_Wm_Icon_Photo
+   --  tk-wm.ads:238:4:Icon_Photo:Test_Wm_Icon_Photo
 
    procedure Test_Icon_Position_1b8212_0e4e31(Gnattest_T: in out Test);
-   --  tk-wm.ads:242:4:Icon_Position:Test_Wm_Icon_Position
+   --  tk-wm.ads:243:4:Icon_Position:Test_Wm_Icon_Position
 
    procedure Test_Icon_Position_277e3f_ebaaaa(Gnattest_T: in out Test);
-   --  tk-wm.ads:246:4:Icon_Position:Test_Wm_Icon_Position2
+   --  tk-wm.ads:247:4:Icon_Position:Test_Wm_Icon_Position2
 
    procedure Test_Icon_Window_6943ef_44e6ae(Gnattest_T: in out Test);
-   --  tk-wm.ads:250:4:Icon_Window:Test_Wm_Icon_Window
+   --  tk-wm.ads:251:4:Icon_Window:Test_Wm_Icon_Window
 
    procedure Test_Icon_Window_7f7484_c90d51(Gnattest_T: in out Test);
-   --  tk-wm.ads:254:4:Icon_Window:Test_Wm_Icon_Window2
+   --  tk-wm.ads:255:4:Icon_Window:Test_Wm_Icon_Window2
 
 end Tk.Wm.Test_Data.Tests;
 --  end read only
