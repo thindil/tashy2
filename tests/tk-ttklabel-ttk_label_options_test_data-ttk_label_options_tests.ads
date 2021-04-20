4418dca4864ac13f46d2c6558d59c018c996f8fe
thindil <thindil@laeran.pl>
2021-04-20 05:40:50 +0000

    updated unit tests
    FossilOrigin-Name: 51ed220dd1431e4741b064e9178c013df66463cc5e3a8d0d87971cae9fcf4ebd

--- tests/tk-ttklabel-ttk_label_options_test_data-ttk_label_options_tests.ads
+++ tests/tk-ttklabel-ttk_label_options_test_data-ttk_label_options_tests.ads
@@ -14,19 +14,19 @@ package Tk.TtkLabel.Ttk_Label_Options_Test_Data.Ttk_Label_Options_Tests is
 
    procedure Test_Create_32e405_36fa40
      (Gnattest_T: in out Test_Ttk_Label_Options);
-   --  tk-ttklabel.ads:112:4:Create:Test_Create_TtkLabel1
+   --  tk-ttklabel.ads:114:4:Create:Test_Create_TtkLabel1
 
    procedure Test_Create_ebbdc1_243d10
      (Gnattest_T: in out Test_Ttk_Label_Options);
-   --  tk-ttklabel.ads:147:4:Create:Test_Create_TtkLabel2
+   --  tk-ttklabel.ads:149:4:Create:Test_Create_TtkLabel2
 
    procedure Test_Get_Options_ded36e_b31868
      (Gnattest_T: in out Test_Ttk_Label_Options);
-   --  tk-ttklabel.ads:172:4:Get_Options:Test_Get_Options_TtkLabel
+   --  tk-ttklabel.ads:174:4:Get_Options:Test_Get_Options_TtkLabel
 
    procedure Test_Configure_0076be_452ca7
      (Gnattest_T: in out Test_Ttk_Label_Options);
-   --  tk-ttklabel.ads:193:4:Configure:Test_Configure_TtkLabel
+   --  tk-ttklabel.ads:195:4:Configure:Test_Configure_TtkLabel
 
 end Tk.TtkLabel.Ttk_Label_Options_Test_Data.Ttk_Label_Options_Tests;
 --  end read only
