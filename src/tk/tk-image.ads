23e5aa06a0377a3055432aae46ae246d89bb536c
thindil <thindil@laeran.pl>
2021-04-23 05:05:01 +0000

    added code documenation
    FossilOrigin-Name: 0fdbcfe76e72b9036eb6fd9a9f018ac552c1ab9a42adf4cda951126474c50196

--- src/tk/tk-image.ads
+++ src/tk/tk-image.ads
@@ -21,18 +21,62 @@ with Tcl.Lists; use Tcl.Lists;
 package Tk.Image is
 -- ****
 
+   --## rule off REDUCEABLE_SCOPE
+   -- ****t* Image/Image.Tk_Image
+   -- FUNCTION
+   -- The Tk identifier of the button
+   -- HISTORY
+   -- 8.6.0 - Added
+   -- SOURCE
    subtype Tk_Image is String;
+   -- ****
 
+   -- ****f* Tk.Image/Image.Delete_(single_image)
+   -- FUNCTION
+   -- Delete the selected Tk image
+   -- PARAMETERS
+   -- Image_Name  - The name of the Tk image to delete
+   -- Interpreter - Tcl interpreter on which the image will be deleted. Can
+   --               be empty. Default value is the default Tcl interpreter
+   -- HISTORY
+   -- 8.6.0 - Added
+   -- EXAMPLE
+   -- -- Delete Tk image named mybitmap on default Tcl interpreter
+   -- Delete("mybitmap");
+   -- SEE ALSO
+   -- Delete_(many_images)
+   -- COMMANDS
+   -- image delete Image_Name
+   -- SOURCE
    procedure Delete
      (Image_Name: Tk_Image;
       Interpreter: Tcl_Interpreter := Get_Interpreter) with
       Pre => Image_Name'Length > 0 and Interpreter /= Null_Interpreter,
       Test_Case => (Name => "Test_Image_Delete", Mode => Nominal);
+      -- ****
 
+   -- ****f* Tk.Image/Image.Delete_(many_images)
+   -- FUNCTION
+   -- Delete the selected Tk images
+   -- PARAMETERS
+   -- Images      - The list of names of the Tk images to delete
+   -- Interpreter - Tcl interpreter on which the images will be deleted. Can
+   --               be empty. Default value is the default Tcl interpreter
+   -- HISTORY
+   -- 8.6.0 - Added
+   -- EXAMPLE
+   -- -- Delete Tk images named mybitmap and mybitmap2 on default Tcl interpreter
+   -- Delete(Array_List'(To_Tcl_String("mybitmap"), To_Tcl_String("mybitmap2")));
+   -- SEE ALSO
+   -- Delete_(single_image)
+   -- COMMANDS
+   -- image delete Images
+   -- SOURCE
    procedure Delete
      (Images: Array_List; Interpreter: Tcl_Interpreter := Get_Interpreter) with
       Pre => Images'Length > 0 and Interpreter /= Null_Interpreter,
       Test_Case => (Name => "Test_Image_Delete2", Mode => Nominal);
+      -- ****
 
    function Height
      (Image_Name: Tk_Image; Interpreter: Tcl_Interpreter := Get_Interpreter)
@@ -66,5 +110,6 @@ package Tk.Image is
       return Natural with
       Pre => Image_Name'Length > 0 and Interpreter /= Null_Interpreter,
       Test_Case => (Name => "Test_Image_Width", Mode => Nominal);
+   --## rule on REDUCEABLE_SCOPE
 
 end Tk.Image;
