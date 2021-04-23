ac30e52bfbecff044c499e21e7a3e2da417a4a88
thindil <thindil@laeran.pl>
2021-04-23 05:20:47 +0000

    updated unit tests
    FossilOrigin-Name: eee621b592e7856c8dfc81c1a4c3219b0ce887e1dfab918df58a7dcc93a766d8

--- tests/tk-image-test_data-tests.ads
+++ tests/tk-image-test_data-tests.ads
@@ -11,28 +11,28 @@ package Tk.Image.Test_Data.Tests is
    null record;
 
    procedure Test_Delete_18cc03_780c83(Gnattest_T: in out Test);
-   --  tk-image.ads:26:4:Delete:Test_Image_Delete
+   --  tk-image.ads:51:4:Delete:Test_Image_Delete
 
    procedure Test_Delete_c4bccc_dad5cd(Gnattest_T: in out Test);
-   --  tk-image.ads:32:4:Delete:Test_Image_Delete2
+   --  tk-image.ads:75:4:Delete:Test_Image_Delete2
 
    procedure Test_Height_80fc6b_1986ce(Gnattest_T: in out Test);
-   --  tk-image.ads:37:4:Height:Test_Image_Height
+   --  tk-image.ads:99:4:Height:Test_Image_Height
 
    procedure Test_In_Use_ce6448_db6496(Gnattest_T: in out Test);
-   --  tk-image.ads:43:4:In_Use:Test_Image_In_Use
+   --  tk-image.ads:121:4:In_Use:Test_Image_In_Use
 
    procedure Test_Names_eadae1_91e953(Gnattest_T: in out Test);
-   --  tk-image.ads:49:4:Names:Test_Image_Names
+   --  tk-image.ads:143:4:Names:Test_Image_Names
 
    procedure Test_Image_Type_2e187d_7648ef(Gnattest_T: in out Test);
-   --  tk-image.ads:54:4:Image_Type:Test_Image_Type
+   --  tk-image.ads:164:4:Image_Type:Test_Image_Type
 
    procedure Test_Types_efe030_6aacf5(Gnattest_T: in out Test);
-   --  tk-image.ads:60:4:Types:Test_Image_Types
+   --  tk-image.ads:186:4:Types:Test_Image_Types
 
    procedure Test_Width_f67773_737c00(Gnattest_T: in out Test);
-   --  tk-image.ads:64:4:Width:Test_Image_Width
+   --  tk-image.ads:209:4:Width:Test_Image_Width
 
 end Tk.Image.Test_Data.Tests;
 --  end read only
