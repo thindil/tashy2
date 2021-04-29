00a976d79d0208212bd911631e7af66121b9c942
thindil <thindil@laeran.pl>
2021-04-29 05:12:02 +0000

    updated unit tests
    FossilOrigin-Name: 5061d223c391dbfc1f874970da48c8a7cc892015d12ae238c68323810ad53906

---
+++ tests/tk-ttkframe-ttk_frame_options_test_data-ttk_frame_options_tests.ads
@@ -0,0 +1,32 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with GNATtest_Generated;
+
+package Tk.TtkFrame.Ttk_Frame_Options_Test_Data.Ttk_Frame_Options_Tests is
+
+   type Test_Ttk_Frame_Options is new GNATtest_Generated.GNATtest_Standard.Tk
+     .TtkFrame
+     .Ttk_Frame_Options_Test_Data
+     .Test_Ttk_Frame_Options with
+   null record;
+
+   procedure Test_Configure_0076be_380f02
+     (Gnattest_T: in out Test_Ttk_Frame_Options);
+   --  tk-ttkframe.ads:75:4:Configure:Test_Configure_Frame
+
+   procedure Test_Create_32e405_f18995
+     (Gnattest_T: in out Test_Ttk_Frame_Options);
+   --  tk-ttkframe.ads:101:4:Create:Test_Create_Frame1
+
+   procedure Test_Create_ebbdc1_94cd82
+     (Gnattest_T: in out Test_Ttk_Frame_Options);
+   --  tk-ttkframe.ads:134:4:Create:Test_Create_Frame2
+
+   procedure Test_Get_Options_ded36e_681455
+     (Gnattest_T: in out Test_Ttk_Frame_Options);
+   --  tk-ttkframe.ads:159:4:Get_Options:Test_Get_Options_Frame
+
+end Tk.TtkFrame.Ttk_Frame_Options_Test_Data.Ttk_Frame_Options_Tests;
+--  end read only
