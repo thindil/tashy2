90232bd83c3da1aec2ab9c4b48da662059846740
thindil <thindil@laeran.pl>
2021-04-30 05:42:42 +0000

    added Option_Image for Anchor_Directions type
    FossilOrigin-Name: c844f6d078964af6ee8821c16bfc192b7e814ba5fad78967b7f5aec808a01e79

--- src/tk/tk-widget.adb
+++ src/tk/tk-widget.adb
@@ -286,6 +286,19 @@ package body Tk.Widget is
       end if;
    end Option_Image;
 
+   procedure Option_Image
+     (Name: String; Value: Anchor_Directions;
+      Options_String: in out Unbounded_String) is
+   begin
+      if Value /= NONE then
+         Append
+           (Source => Options_String,
+            New_Item =>
+              " -" & Name & " " &
+              To_Lower(Item => Anchor_Directions'Image(Value)));
+      end if;
+   end Option_Image;
+
    function Option_Value(Widgt: Tk_Widget; Name: String) return Tcl_String is
    begin
       return To_Tcl_String
