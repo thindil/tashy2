587000e2aa0095b1350452062e31e7d6f95540b4
thindil <thindil@laeran.pl>
2021-05-14 05:45:44 +0000

    remove button when tests fails
    FossilOrigin-Name: f289c152d773cfd6301dd315c7e997e5d38eb415e46f8d95d90a255665d03fc2

--- tests/tk-ttkbutton-ttk_button_options_test_data.adb
+++ tests/tk-ttkbutton-ttk_button_options_test_data.adb
@@ -23,6 +23,7 @@ package body Tk.TtkButton.Ttk_Button_Options_Test_Data is
    end Set_Up;
 
    procedure Tear_Down(Gnattest_T: in out Test_Ttk_Button_Options) is
+      Button: Ttk_Button;
    begin
       GNATtest_Generated.GNATtest_Standard.Tk.TtkWidget
         .Ttk_Widget_Options_Test_Data
@@ -33,6 +34,10 @@ package body Tk.TtkButton.Ttk_Button_Options_Test_Data is
            .Ttk_Widget_Options_Tests
            .Test_Ttk_Widget_Options
            (Gnattest_T));
+      Button := Get_Widget(".mybutton");
+      if Button /= Null_Widget then
+         Destroy(Button);
+      end if;
    end Tear_Down;
 
 end Tk.TtkButton.Ttk_Button_Options_Test_Data;
