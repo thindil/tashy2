cddc752f1313b0b873186e4f5a610d473acb6ff7
thindil <thindil@laeran.pl>
2021-05-27 05:53:37 +0000

    updated unit tests
    FossilOrigin-Name: 96a17c9f56ce716a5cc6876efda8343015494498582ada023d2767c0efbfd3b6

--- tests/tcl-info-test_data-tests.ads
+++ tests/tcl-info-test_data-tests.ads
@@ -11,65 +11,65 @@ package Tcl.Info.Test_Data.Tests is
    null record;
 
    procedure Test_Get_Arguments_58c9c6_2155d3(Gnattest_T: in out Test);
-   --  tcl-info.ads:40:4:Get_Arguments:Test_Info_Arguments
+   --  tcl-info.ads:42:4:Get_Arguments:Test_Info_Arguments
 
    procedure Test_Get_Procedure_Body_4ab4c1_b86b33(Gnattest_T: in out Test);
-   --  tcl-info.ads:68:4:Get_Procedure_Body:Test_Info_Procedure_Body
+   --  tcl-info.ads:72:4:Get_Procedure_Body:Test_Info_Procedure_Body
 
    procedure Test_Get_Commands_Count_9e53d6_cae496(Gnattest_T: in out Test);
-   --  tcl-info.ads:93:4:Get_Commands_Count:Test_Info_Commands_Count
+   --  tcl-info.ads:99:4:Get_Commands_Count:Test_Info_Commands_Count
 
    procedure Test_Get_Commands_f2ad9c_675616(Gnattest_T: in out Test);
-   --  tcl-info.ads:116:4:Get_Commands:Test_Info_Command
+   --  tcl-info.ads:124:4:Get_Commands:Test_Info_Command
 
    procedure Test_Complete_539361_79b807(Gnattest_T: in out Test);
-   --  tcl-info.ads:145:4:Complete:Test_Info_Complete
+   --  tcl-info.ads:155:4:Complete:Test_Info_Complete
 
    procedure Test_Get_Coroutine_df9ca4_613b2a(Gnattest_T: in out Test);
-   --  tcl-info.ads:170:4:Get_Coroutine:Test_Info_Coroutine
+   --  tcl-info.ads:182:4:Get_Coroutine:Test_Info_Coroutine
 
    procedure Test_Get_Default_2d3d83_db99d7(Gnattest_T: in out Test);
-   --  tcl-info.ads:201:4:Get_Default:Test_Info_Default
+   --  tcl-info.ads:214:4:Get_Default:Test_Info_Default
 
    procedure Test_Get_Error_Stack_c33442_123ed2(Gnattest_T: in out Test);
-   --  tcl-info.ads:227:4:Get_Error_Stack:Test_Info_ErrorStack
+   --  tcl-info.ads:242:4:Get_Error_Stack:Test_Info_ErrorStack
 
    procedure Test_Exists_e0ba6a_a2ca41(Gnattest_T: in out Test);
-   --  tcl-info.ads:250:4:Exists:Test_Info_Exists
+   --  tcl-info.ads:267:4:Exists:Test_Info_Exists
 
    procedure Test_Get_Functions_5a1dbc_10dc33(Gnattest_T: in out Test);
-   --  tcl-info.ads:279:4:Get_Functions:Test_Info_Functions
+   --  tcl-info.ads:298:4:Get_Functions:Test_Info_Functions
 
    procedure Test_Get_Globals_1284d9_c19ad0(Gnattest_T: in out Test);
-   --  tcl-info.ads:311:4:Get_Globals:Test_Info_Globals
+   --  tcl-info.ads:332:4:Get_Globals:Test_Info_Globals
 
    procedure Test_Get_Host_Name_0a278d_b2a918(Gnattest_T: in out Test);
-   --  tcl-info.ads:339:4:Get_Host_Name:Test_Info_HostName
+   --  tcl-info.ads:362:4:Get_Host_Name:Test_Info_HostName
 
    procedure Test_Get_Library_36938c_8b9a20(Gnattest_T: in out Test);
-   --  tcl-info.ads:361:4:Get_Library:Test_Info_Library
+   --  tcl-info.ads:386:4:Get_Library:Test_Info_Library
 
    procedure Test_Get_Locals_09a275_f4c43e(Gnattest_T: in out Test);
-   --  tcl-info.ads:387:4:Get_Locals:Test_Info_Locals
+   --  tcl-info.ads:414:4:Get_Locals:Test_Info_Locals
 
    procedure Test_Get_Name_Of_Executable_1cc16d_24ff4c
      (Gnattest_T: in out Test);
-   --  tcl-info.ads:413:4:Get_Name_Of_Executable:Test_Info_Name_Of_Executable
+   --  tcl-info.ads:442:4:Get_Name_Of_Executable:Test_Info_Name_Of_Executable
 
    procedure Test_Get_Patch_Level_0baa9d_325136(Gnattest_T: in out Test);
-   --  tcl-info.ads:437:4:Get_Patch_Level:Test_Info_Patch_Level
+   --  tcl-info.ads:468:4:Get_Patch_Level:Test_Info_Patch_Level
 
    procedure Test_Get_Procedures_848b43_884500(Gnattest_T: in out Test);
-   --  tcl-info.ads:465:4:Get_Procedures:Test_Info_Procs
+   --  tcl-info.ads:498:4:Get_Procedures:Test_Info_Procs
 
    procedure Test_Get_Script_86aa75_ddce00(Gnattest_T: in out Test);
-   --  tcl-info.ads:495:4:Get_Script:Test_Info_Script
+   --  tcl-info.ads:530:4:Get_Script:Test_Info_Script
 
    procedure Test_Get_Tcl_Version_d04078_6661d4(Gnattest_T: in out Test);
-   --  tcl-info.ads:520:4:Get_Tcl_Version:Test_Info_Tcl_Version
+   --  tcl-info.ads:557:4:Get_Tcl_Version:Test_Info_Tcl_Version
 
    procedure Test_Get_Variables_fedbab_9d2a56(Gnattest_T: in out Test);
-   --  tcl-info.ads:547:4:Get_Variables:Test_Info_Vars
+   --  tcl-info.ads:586:4:Get_Variables:Test_Info_Vars
 
 end Tcl.Info.Test_Data.Tests;
 --  end read only
