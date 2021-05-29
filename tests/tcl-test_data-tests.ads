9ec93b2d25654497fdcfd20dbb077a23206dc38b
thindil <thindil@laeran.pl>
2021-05-29 05:49:37 +0000

    updated unit tests
    FossilOrigin-Name: c8aea5b42f4d299e207a9a2afa266dab15b5652cfc72bd241dd8021d6a54e75e

--- tests/tcl-test_data-tests.ads
+++ tests/tcl-test_data-tests.ads
@@ -11,25 +11,22 @@ package Tcl.Test_Data.Tests is
    null record;
 
    procedure Test_Tcl_Eval_6f41cd_5b9cd5(Gnattest_T: in out Test);
-   --  tcl.ads:138:4:Tcl_Eval:Test_Tcl_Eval
+   --  tcl.ads:145:4:Tcl_Eval:Test_Tcl_Eval
 
    procedure Test_Tcl_Eval_907c70_916b02(Gnattest_T: in out Test);
-   --  tcl.ads:162:4:Tcl_Eval:Test_Tcl_Eval2
+   --  tcl.ads:169:4:Tcl_Eval:Test_Tcl_Eval2
 
    procedure Test_Tcl_Eval_8c4419_19bef1(Gnattest_T: in out Test);
-   --  tcl.ads:187:4:Tcl_Eval:Test_Tcl_Eval3
+   --  tcl.ads:194:4:Tcl_Eval:Test_Tcl_Eval3
 
    procedure Test_Tcl_Get_Result_9a7ac3_b83d43(Gnattest_T: in out Test);
-   --  tcl.ads:307:4:Tcl_Get_Result:Test_Tcl_GetResult
+   --  tcl.ads:314:4:Tcl_Get_Result:Test_Tcl_GetResult
 
    procedure Test_Tcl_Set_Result_2e8975_cb8f85(Gnattest_T: in out Test);
-   --  tcl.ads:374:4:Tcl_Set_Result:Test_Tcl_SetResult
+   --  tcl.ads:381:4:Tcl_Set_Result:Test_Tcl_SetResult
 
    procedure Test_Tcl_Update_7113e2_953c64(Gnattest_T: in out Test);
-   --  tcl.ads:399:4:Tcl_Update:Test_Tcl_Update
-
-   procedure Test_Set_Debug_4bc828_f8ec5d(Gnattest_T: in out Test);
-   --  tcl.ads:419:4:Set_Debug:Test_Debug
+   --  tcl.ads:406:4:Tcl_Update:Test_Tcl_Update
 
 end Tcl.Test_Data.Tests;
 --  end read only
