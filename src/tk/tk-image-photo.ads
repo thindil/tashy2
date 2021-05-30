a13a9754c51370e6bd6a1d710a028e944d3b8784
thindil <thindil@laeran.pl>
2021-05-30 05:17:27 +0000

    continue work on bindings for Tk image photo commands
    FossilOrigin-Name: dda1ffa1f95fcd967901e6e78e2553032156d8ec7fd38a26bbd3c02c15eca6cb

--- src/tk/tk-image-photo.ads
+++ src/tk/tk-image-photo.ads
@@ -16,12 +16,63 @@ with Tk.Widget; use Tk.Widget;
 
 package Tk.Image.Photo is
 
-   type Photo_Options is new Image_Options with record
+   type Photo_Formats is (PNG, GIF, OTHER);
+
+   type Photo_Options(Photo_Format: Photo_Formats) is new Image_Options with
+   record
       Format: Tcl_String;
       Gamma: Positive_Float;
       Height: Natural;
       Palette: Tcl_String;
       Width: Natural;
+      case Photo_Format is
+         when PNG =>
+            Alpha: Positive_Float range -1.0 .. 1.0;
+         when GIF =>
+            Index: Natural;
+         when OTHER =>
+            null;
+      end case;
    end record;
+
+   -- ****f* Photo/Photo.Create
+   -- FUNCTION
+   -- Create a new Tk image of photo type with the selected name and options
+   -- PARAMETERS
+   -- Name        - Tk name for the newly created image
+   -- Options     - Options for the newly created image
+   -- Interpreter - Tcl interpreter on which the image will be created. Can
+   --               be empty. Default value is the default Tcl interpreter
+   -- HISTORY
+   -- 8.6.0 - Added
+   -- EXAMPLE
+   -- -- Create the image with name .myimage from file myimage.png
+   -- Create(".myimage", Photo_Options'(Photo_Format => PNG, File => "myimage.png", others => <>));
+   -- COMMANDS
+   -- image create photo Name Options
+   -- SOURCE
+   procedure Create
+     (Name: String; Options: Photo_Options;
+      Interpreter: Tcl_Interpreter := Get_Interpreter) with
+      Pre => Name'Length > 0 and Interpreter /= Null_Interpreter,
+      Test_Case => (Name => "Test_Create_Photo", Mode => Nominal);
+   -- ****
+
+   procedure Blank
+     (Name: String; Interpreter: Tcl_Interpreter := Get_Interpreter) with
+      Pre => Name'Length > 0 and Interpreter /= Null_Interpreter,
+      Test_Case => (Name => "Test_Blank_Photo", Mode => Nominal);
+
+   procedure Configure
+     (Name: String; Options: Photo_Options;
+      Interpreter: Tcl_Interpreter := Get_Interpreter) with
+      Pre => Name'Length > 0 and Interpreter /= Null_Interpreter,
+      Test_Case => (Name => "Test_Configure_Photo", Mode => Nominal);
+
+   function Get_Options
+     (Name: String; Interpreter: Tcl_Interpreter := Get_Interpreter)
+      return Photo_Options with
+      Pre => Name'Length > 0 and Interpreter /= Null_Interpreter,
+      Test_Case => (Name => "Test_Get_Options_Photo", Mode => Nominal);
 
 end Tk.Image.Photo;
