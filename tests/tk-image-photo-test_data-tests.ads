a93ef3e52c5fb3ce459a2e9c4a02225170b9786a
thindil <thindil@laeran.pl>
2021-06-01 05:57:23 +0000

    updated unit tests
    FossilOrigin-Name: 253abc595eb97e2c1342fa28769b6643fbfa45d5aaf1e50d6e387013a6ab92e4

--- tests/tk-image-photo-test_data-tests.ads
+++ tests/tk-image-photo-test_data-tests.ads
@@ -12,10 +12,25 @@ package Tk.Image.Photo.Test_Data.Tests is
    null record;
 
    procedure Test_Blank_2c1442_1f34c5(Gnattest_T: in out Test);
-   --  tk-image-photo.ads:68:4:Blank:Tests_Blank_Photo
+   --  tk-image-photo.ads:76:4:Blank:Tests_Blank_Photo
 
    procedure Test_Copy_c44a35_21de2d(Gnattest_T: in out Test);
-   --  tk-image-photo.ads:85:4:Copy:Tests_Copy_Photo
+   --  tk-image-photo.ads:93:4:Copy:Tests_Copy_Photo
+
+   procedure Test_Get_Data_9275f9_ea4492(Gnattest_T: in out Test);
+   --  tk-image-photo.ads:101:4:Get_Data:Tests_Get_Data_Photo
+
+   procedure Test_Get_Color_6fd571_e50560(Gnattest_T: in out Test);
+   --  tk-image-photo.ads:109:4:Get_Color:Tests_Get_Color_Photo
+
+   procedure Test_Put_Data_f479a7_30a8e8(Gnattest_T: in out Test);
+   --  tk-image-photo.ads:115:4:Put_Data:Tests_Put_Data_Photo
+
+   procedure Test_Read_a3304e_49c9ca(Gnattest_T: in out Test);
+   --  tk-image-photo.ads:123:4:Read:Tests_Read_Photo
+
+   procedure Test_Redither_68a59f_5db920(Gnattest_T: in out Test);
+   --  tk-image-photo.ads:132:4:Redither:Tests_Redither_Photo
 
 end Tk.Image.Photo.Test_Data.Tests;
 --  end read only
