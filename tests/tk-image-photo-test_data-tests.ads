7309c551cb29329f082836d3a50eb71fa7a2e673
thindil <thindil@laeran.pl>
2021-06-08 06:00:45 +0000

    updated unit tests
    FossilOrigin-Name: a15331f2501f2152d64758d28483cd37d0a7c8efc51a847c4058b28d9c8c86d9

--- tests/tk-image-photo-test_data-tests.ads
+++ tests/tk-image-photo-test_data-tests.ads
@@ -12,37 +12,37 @@ package Tk.Image.Photo.Test_Data.Tests is
    null record;
 
    procedure Test_Blank_2c1442_30601f(Gnattest_T: in out Test);
-   --  tk-image-photo.ads:78:4:Blank:Tests_Blank_Photo
+   --  tk-image-photo.ads:82:4:Blank:Tests_Blank_Photo
 
    procedure Test_Get_Option_e3d52c_8f9fe9(Gnattest_T: in out Test);
-   --  tk-image-photo.ads:90:4:Get_Option:Tests_Get_Option_Photo
+   --  tk-image-photo.ads:94:4:Get_Option:Tests_Get_Option_Photo
 
-   procedure Test_Copy_c44a35_21de2d(Gnattest_T: in out Test);
-   --  tk-image-photo.ads:106:4:Copy:Tests_Copy_Photo
+   procedure Test_Copy_0a35cf_21de2d(Gnattest_T: in out Test);
+   --  tk-image-photo.ads:110:4:Copy:Tests_Copy_Photo
 
    procedure Test_Get_Data_0223c5_8ba84f(Gnattest_T: in out Test);
-   --  tk-image-photo.ads:114:4:Get_Data:Tests_Get_Data_Photo
+   --  tk-image-photo.ads:120:4:Get_Data:Tests_Get_Data_Photo
 
    procedure Test_Get_Color_6fd571_5f6b3a(Gnattest_T: in out Test);
-   --  tk-image-photo.ads:121:4:Get_Color:Tests_Get_Color_Photo
+   --  tk-image-photo.ads:127:4:Get_Color:Tests_Get_Color_Photo
 
    procedure Test_Put_Data_1f94ae_ee5695(Gnattest_T: in out Test);
-   --  tk-image-photo.ads:127:4:Put_Data:Tests_Put_Data_Photo
+   --  tk-image-photo.ads:133:4:Put_Data:Tests_Put_Data_Photo
 
    procedure Test_Read_9efbd6_24b510(Gnattest_T: in out Test);
-   --  tk-image-photo.ads:135:4:Read:Tests_Read_Photo
+   --  tk-image-photo.ads:141:4:Read:Tests_Read_Photo
 
    procedure Test_Redither_68a59f_4f3041(Gnattest_T: in out Test);
-   --  tk-image-photo.ads:144:4:Redither:Tests_Redither_Photo
+   --  tk-image-photo.ads:150:4:Redither:Tests_Redither_Photo
 
    procedure Test_Get_Transparency_745b2d_ffe137(Gnattest_T: in out Test);
-   --  tk-image-photo.ads:150:4:Get_Transparency:Tests_Get_Transparency_Photo
+   --  tk-image-photo.ads:156:4:Get_Transparency:Tests_Get_Transparency_Photo
 
    procedure Test_Set_Transparency_76d5d1_e76ade(Gnattest_T: in out Test);
-   --  tk-image-photo.ads:156:4:Set_Transparency:Tests_Set_Transparency_Photo
+   --  tk-image-photo.ads:162:4:Set_Transparency:Tests_Set_Transparency_Photo
 
    procedure Test_Write_f4e43e_96f97b(Gnattest_T: in out Test);
-   --  tk-image-photo.ads:162:4:Write:Tests_Write_Photo
+   --  tk-image-photo.ads:168:4:Write:Tests_Write_Photo
 
 end Tk.Image.Photo.Test_Data.Tests;
 --  end read only
