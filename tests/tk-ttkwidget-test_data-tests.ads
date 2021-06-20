4417e270a3ea2d3052d4d66e933869914362174e
thindil <thindil@laeran.pl>
2021-06-20 05:08:12 +0000

    updated unit tests
    FossilOrigin-Name: 33b0f96bc558b3775952a852698cf0a95aa3c55d30905cd86e8ef186a9a309a9

--- tests/tk-ttkwidget-test_data-tests.ads
+++ tests/tk-ttkwidget-test_data-tests.ads
@@ -11,40 +11,40 @@ package Tk.TtkWidget.Test_Data.Tests is
    null record;
 
    procedure Test_Option_Image_a3facf_ddcb4f(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:250:4:Option_Image:Test_Option_Image_Compound_Type
+   --  tk-ttkwidget.ads:252:4:Option_Image:Test_Option_Image_Compound_Type
 
    procedure Test_Option_Image_5ac754_8ca289(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:256:4:Option_Image:Test_Option_Image_Distabled_State_Type
+   --  tk-ttkwidget.ads:258:4:Option_Image:Test_Option_Image_Distabled_State_Type
 
    procedure Test_Option_Image_a0749a_9522a2(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:262:4:Option_Image:Test_Option_Image_Image_Option
+   --  tk-ttkwidget.ads:264:4:Option_Image:Test_Option_Image_Image_Option
 
    procedure Test_Option_Image_fac0b4_5f511f(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:267:4:Option_Image:Test_Option_Image_Padding_Data
+   --  tk-ttkwidget.ads:269:4:Option_Image:Test_Option_Image_Padding_Data
 
    procedure Test_Option_Value_5bd68b_a92add(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:290:4:Option_Value:Test_Option_Value_Compound_Type
+   --  tk-ttkwidget.ads:292:4:Option_Value:Test_Option_Value_Compound_Type
 
    procedure Test_Option_Value_e44442_1318d9(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:295:4:Option_Value:Test_Option_Value_Disabled_State_Type
+   --  tk-ttkwidget.ads:297:4:Option_Value:Test_Option_Value_Disabled_State_Type
 
    procedure Test_Option_Value_7d1d95_d2d94b(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:300:4:Option_Value:Test_Option_Value_Ttk_Image_Option
+   --  tk-ttkwidget.ads:302:4:Option_Value:Test_Option_Value_Ttk_Image_Option
 
    procedure Test_Option_Value_08ea7f_956d9f(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:305:4:Option_Value:Test_Option_Value_Padding_Data
+   --  tk-ttkwidget.ads:307:4:Option_Value:Test_Option_Value_Padding_Data
 
    procedure Test_In_State_46b68b_db2848(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:330:4:In_State:Test_Ttk_Widget_In_State
+   --  tk-ttkwidget.ads:332:4:In_State:Test_Ttk_Widget_In_State
 
    procedure Test_In_State_318f72_d83e8c(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:355:4:In_State:Test_Ttk_Widget_In_State2
+   --  tk-ttkwidget.ads:357:4:In_State:Test_Ttk_Widget_In_State2
 
    procedure Test_Set_State_86b648_15a1a7(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:380:4:Set_State:Test_Ttk_Widget_State
+   --  tk-ttkwidget.ads:382:4:Set_State:Test_Ttk_Widget_State
 
    procedure Test_Get_States_734267_5a6282(Gnattest_T: in out Test);
-   --  tk-ttkwidget.ads:404:4:Get_States:Test_Ttk_Widget_State2
+   --  tk-ttkwidget.ads:406:4:Get_States:Test_Ttk_Widget_State2
 
 end Tk.TtkWidget.Test_Data.Tests;
 --  end read only
