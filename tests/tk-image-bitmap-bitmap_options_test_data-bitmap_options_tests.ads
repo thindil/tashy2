81c5b98d3104a880ee79737e85f0601410682995
thindil <thindil@laeran.pl>
2021-06-21 05:30:09 +0000

    updated unit tests
    FossilOrigin-Name: 6eb087278379fe231f01c628e967f01ca8b7339d3334c84b14588a13600fe723

--- tests/tk-image-bitmap-bitmap_options_test_data-bitmap_options_tests.ads
+++ tests/tk-image-bitmap-bitmap_options_test_data-bitmap_options_tests.ads
@@ -14,18 +14,18 @@ package Tk.Image.Bitmap.Bitmap_Options_Test_Data.Bitmap_Options_Tests is
    null record;
 
    procedure Test_Create_22037c_8bd2c6(Gnattest_T: in out Test_Bitmap_Options);
-   --  tk-image-bitmap.ads:66:4:Create:Tests_Create_Bitmap
+   --  tk-image-bitmap.ads:67:4:Create:Tests_Create_Bitmap
 
    procedure Test_Create_fa334a_afeffb(Gnattest_T: in out Test_Bitmap_Options);
-   --  tk-image-bitmap.ads:92:4:Create:Tests_Create2_Bitmap
+   --  tk-image-bitmap.ads:93:4:Create:Tests_Create2_Bitmap
 
    procedure Test_Configure_6e2ac0_59fc30
      (Gnattest_T: in out Test_Bitmap_Options);
-   --  tk-image-bitmap.ads:116:4:Configure:Tests_Configure_Bitmap
+   --  tk-image-bitmap.ads:117:4:Configure:Tests_Configure_Bitmap
 
    procedure Test_Get_Options_5c7a9c_1a3561
      (Gnattest_T: in out Test_Bitmap_Options);
-   --  tk-image-bitmap.ads:175:4:Get_Options:Tests_Get_Options_Bitmap
+   --  tk-image-bitmap.ads:176:4:Get_Options:Tests_Get_Options_Bitmap
 
 end Tk.Image.Bitmap.Bitmap_Options_Test_Data.Bitmap_Options_Tests;
 --  end read only
