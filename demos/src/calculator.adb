8098ea06c4a1a64c39978c977100686e0f9b69b4
thindil <thindil@laeran.pl>
2021-06-23 05:40:18 +0000

    started work on main menu for demo window
    FossilOrigin-Name: 1a0fd7f45d11a653ea1282d047c1dbf10c206dcc238a2e707f98043183a86004

--- demos/src/calculator.adb
+++ demos/src/calculator.adb
@@ -4,10 +4,13 @@ with Tk; use Tk;
 with Tk.Button; use Tk.Button;
 with Tk.Grid; use Tk.Grid;
 with Tk.MainWindow; use Tk.MainWindow;
+with Tk.Menu; use Tk.Menu;
+with Tk.TopLevel; use Tk.TopLevel;
 with Tk.Wm; use Tk.Wm;
 
 procedure Calculator is
    Button: Tk_Button;
+   Main_Window: Tk_Toplevel;
 begin
    -- Initialize Tcl interpreter
    Tcl_Init(Interpreter => Create_Interpreter);
@@ -15,14 +18,36 @@ begin
    -- Initialize the Tk library
    Tk_Init;
 
+   -- Get the main window of the program
+   Main_Window := Get_Main_Window;
+
    -- Set the size of the main window
-   Set_Geometry(Window => Get_Main_Window, Width => 600, Height => 400);
+   Set_Geometry
+     (Window => Main_Window, Width => 600, Height => 400, X => 0, Y => 0);
+
+   -- Set the title for the main window
+   Set_Title
+     (Window => Main_Window,
+      New_Title => To_Tcl_String("Tashy2 demo - calculator"));
+
+   -- Set the program's simple menu File with one entry Quit to quit from the
+   -- program
+   declare
+      Main_Menu: constant Tk_Menu :=
+        Create(Path_Name => ".mainmenu", Options => Default_Menu_Options);
+   begin
+      Configure
+        (Toplevel_Widget => Main_Window,
+         Options => (Menu => Main_Menu, others => <>));
+      null;
+   end;
 
    -- Create the button with text Quit which will be closing the application
    Create
      (Button, ".button",
-      (Text => To_Tcl_String("Quit"), Command => To_Tcl_String("exit"),
-       others => <>));
+      Button_Options'
+        (Text => To_Tcl_String("Quit"), Command => To_Tcl_String("exit"),
+         others => <>));
    -- Add the button to the main window
    Add(Button);
 
