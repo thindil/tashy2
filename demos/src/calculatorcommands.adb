4a7fdf65d4e823717dab98e11ee1613c3169406b
thindil <thindil@laeran.pl>
2021-07-02 06:08:57 +0000

    moved buttons command to separated package
    FossilOrigin-Name: 92edc77cd01b95f4031e489fd26e5467d2465a72a4bb15db1e644d611b25e75c

---
+++ demos/src/calculatorcommands.adb
@@ -0,0 +1,25 @@
+with Tcl.Strings; use Tcl.Strings;
+with Tk.TtkButton; use Tk.TtkButton;
+with Tk.TtkLabel; use Tk.TtkLabel;
+with Tk.Widget; use Tk.Widget;
+
+package body CalculatorCommands is
+
+   function On_Click
+     (Client_Data: System.Address; Interpreter: Tcl_Interpreter;
+      Argc: Positive; Argv: Argv_Pointer.Pointer) return Tcl_Results is
+      pragma Unreferenced(Client_Data, Argc);
+      Button: constant Ttk_Button :=
+        Get_Widget(Get_Argument(Argv, 1), Interpreter);
+      Display_Label: constant Ttk_Label := Get_Widget(Get_Argument(Argv, 2), Interpreter);
+      Label_Options: constant Ttk_Label_Options := Get_Options(Get_Widget(Get_Argument(Argv, 2), Interpreter));
+      Button_Options: constant Ttk_Button_Options := Get_Options(Button);
+   begin
+      Configure
+        (Label => Display_Label,
+         Options =>
+           (Text => Label_Options.Text & Button_Options.Text, others => <>));
+      return TCL_OK;
+   end On_Click;
+
+end CalculatorCommands;
---
+++ demos/src/calculatorcommands.ads
@@ -0,0 +1,12 @@
+with System;
+with Tcl; use Tcl;
+with Tcl.Commands; use Tcl.Commands;
+
+package CalculatorCommands is
+
+   function On_Click
+     (Client_Data: System.Address; Interpreter: Tcl_Interpreter;
+      Argc: Positive; Argv: Argv_Pointer.Pointer) return Tcl_Results with
+      Convention => C;
+
+end CalculatorCommands;
