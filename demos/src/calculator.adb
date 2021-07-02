c4932d6fe609ab334d31d6b63e38d123a4e61b1b
thindil <thindil@laeran.pl>
2021-07-02 05:33:29 +0000

    added operators buttons to demo
    FossilOrigin-Name: f9274c647ad5a4b6e51b74aa436e4618d867e8880dec240dd9de9abd649f62ba

--- demos/src/calculator.adb
+++ demos/src/calculator.adb
@@ -76,7 +76,7 @@ begin
         (Sticky => E,
          Pad_Y => (Top => (Value => 5.0, Value_Unit => <>), Bottom => <>),
          Pad_X => (Left => (Value => 10.0, Value_Unit => <>), Right => <>),
-         others => <>));
+         Column_Span => 2, others => <>));
    -- Resize the display to the width of the main window
    Column_Configure(Main_Window, Display_Label, (Weight => 1, others => <>));
 
@@ -92,8 +92,9 @@ begin
    begin
       -- Create buttons for numbers from 1 to 9 and add them to their parents
       -- frame
+      Add_Numbers_Buttons_Loop :
       for I in reverse 1 .. 9 loop
-         Button_Text := Trim(Natural'Image(I), Left);
+         Button_Text := Trim(Source => Natural'Image(I), Side => Left);
          Button :=
            Create
              (Path_Name =>
@@ -110,7 +111,7 @@ begin
             Column := 2;
             Row := Row + 1;
          end if;
-      end loop;
+      end loop Add_Numbers_Buttons_Loop;
       -- Create button for number 0 and add it to the parent frame
       Button :=
         Create
@@ -122,8 +123,56 @@ begin
         (Child => Button,
          Options =>
            (Row => Row, Sticky => WIDTH, Column_Span => 3, others => <>));
-      -- Add all buttons to the main window
+      -- Add all numbers buttons to the main window
       Add(Child => Numbers_Frame);
+   end;
+
+   declare
+      -- Create frame for the buttons
+      Operators_Frame: constant Ttk_Frame :=
+        Create
+          (Path_Name => ".operators", Options => Default_Ttk_Frame_Options);
+      Button: Ttk_Button;
+      Column: Extended_Natural := 1;
+      Row: Extended_Natural := 1;
+      Operators: constant array(1 .. 4) of String(1 .. 1) :=
+        ("+", "-", "*", "/");
+   begin
+      Add_Operators_Buttons_Loop :
+      for I in Operators'Range loop
+         Button :=
+           Create
+             (Path_Name =>
+                Tk_Path_Name(Widgt => Operators_Frame) & "." &
+                Trim(Source => Positive'Image(I), Side => Left),
+              Options =>
+                Ttk_Button_Options'
+                  (Text => To_Tcl_String(Source => Operators(I)),
+                   others => <>));
+         Add
+           (Child => Button,
+            Options => (Column => Column, Row => Row, others => <>));
+         Column := Column - 1;
+         if Column = -1 then
+            Column := 1;
+            Row := Row + 1;
+         end if;
+      end loop Add_Operators_Buttons_Loop;
+      -- Create button for equal operator and add it to the parent frame
+      Button :=
+        Create
+          (Path_Name => Tk_Path_Name(Widgt => Operators_Frame) & ".equal",
+           Options =>
+             Ttk_Button_Options'
+               (Text => To_Tcl_String(Source => "="), others => <>));
+      Add
+        (Child => Button,
+         Options =>
+           (Row => Row, Sticky => WIDTH, Column_Span => 2, others => <>));
+      -- Add all operations buttons to the main window
+      Add
+        (Child => Operators_Frame,
+         Options => (Row => 1, Column => 1, Sticky => N, others => <>));
    end;
 
    -- Start the main Tk event loop
