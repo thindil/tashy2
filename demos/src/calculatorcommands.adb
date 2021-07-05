90e4f326b10de3907bc6baae92bf893310ea56db
thindil <thindil@laeran.pl>
2021-07-05 05:01:09 +0000

    some code cleanup
    FossilOrigin-Name: e46beb951c127da7e88f18079e26439acfcc3f667055a1ba3822c0f70425e2c0

--- demos/src/calculatorcommands.adb
+++ demos/src/calculatorcommands.adb
@@ -1,3 +1,4 @@
+with Ada.Strings.Maps; use Ada.Strings.Maps;
 with Tcl.Strings; use Tcl.Strings;
 with Tk.TtkButton; use Tk.TtkButton;
 with Tk.TtkLabel; use Tk.TtkLabel;
@@ -10,16 +11,22 @@ package body CalculatorCommands is
       Argc: Positive; Argv: Argv_Pointer.Pointer) return Tcl_Results is
       pragma Unreferenced(Client_Data, Argc);
       Button: constant Ttk_Button :=
-        Get_Widget(Get_Argument(Argv, 1), Interpreter);
+        Get_Widget
+          (Path_Name => Get_Argument(Arguments_Pointer => Argv, Index => 1),
+           Interpreter => Interpreter);
       Display_Label: constant Ttk_Label :=
-        Get_Widget(Get_Argument(Argv, 2), Interpreter);
+        Get_Widget
+          (Path_Name => Get_Argument(Arguments_Pointer => Argv, Index => 2),
+           Interpreter => Interpreter);
       Label_Options: Ttk_Label_Options := Get_Options(Label => Display_Label);
-      Button_Options: constant Ttk_Button_Options := Get_Options(Button);
+      Button_Options: constant Ttk_Button_Options :=
+        Get_Options(Button => Button);
+      Operators_Set: constant Character_Set := To_Set(Sequence => "*+/-");
    begin
       if Label_Options.Text = To_Tcl_String(Source => "0") then
          Label_Options.Text := Null_Tcl_String;
       end if;
-      if Button_Options.Text = To_Unbounded_String("=") then
+      if Button_Options.Text = To_Unbounded_String(Source => "=") then
          Configure
            (Label => Display_Label,
             Options =>
@@ -45,11 +52,13 @@ package body CalculatorCommands is
       Argc: Positive; Argv: Argv_Pointer.Pointer) return Tcl_Results is
       pragma Unreferenced(Client_Data, Argc);
       Display_Label: constant Ttk_Label :=
-        Get_Widget(Get_Argument(Argv, 1), Interpreter);
+        Get_Widget
+          (Path_Name => Get_Argument(Arguments_Pointer => Argv, Index => 1),
+           Interpreter => Interpreter);
    begin
       Configure
         (Label => Display_Label,
-         Options => (Text => To_Tcl_String("0"), others => <>));
+         Options => (Text => To_Tcl_String(Source => "0"), others => <>));
       return TCL_OK;
    end Clear_Display;
 
