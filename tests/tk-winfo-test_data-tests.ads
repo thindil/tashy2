8b5ecb59cf795e6a5ca832750b57421f75d33b70
thindil <thindil@laeran.pl>
2021-07-11 05:50:53 +0000

    updated unit tests
    FossilOrigin-Name: 94b66c0172ce09fb01a785a4fd74d34c0aef134a26e33de3c7213cdb62ad3d12

---
+++ tests/tk-winfo-test_data-tests.ads
@@ -0,0 +1,29 @@
+--  This package has been generated automatically by GNATtest.
+--  Do not edit any part of it, see GNATtest documentation for more details.
+
+--  begin read only
+with Gnattest_Generated;
+
+package Tk.Winfo.Test_Data.Tests is
+
+   type Test is new GNATtest_Generated.GNATtest_Standard.Tk.Winfo.Test_Data
+     .Test with
+   null record;
+
+   procedure Test_Atom_8a3ab9_da7d59(Gnattest_T: in out Test);
+   --  tk-winfo.ads:25:4:Atom:Test_Winfo_Atom
+
+   procedure Test_Atom_Name_ffa709_5ba3e0(Gnattest_T: in out Test);
+   --  tk-winfo.ads:31:4:Atom_Name:Test_Winfo_Atom_Name
+
+   procedure Test_Cells_13d387_f11487(Gnattest_T: in out Test);
+   --  tk-winfo.ads:36:4:Cells:Test_Winfo_Cells
+
+   procedure Test_Children_87e2ce_9a2e0c(Gnattest_T: in out Test);
+   --  tk-winfo.ads:40:4:Children:Test_Winfo_Children
+
+   procedure Test_Class_1df3af_fa80a1(Gnattest_T: in out Test);
+   --  tk-winfo.ads:44:4:Class:Test_Winfo_Class
+
+end Tk.Winfo.Test_Data.Tests;
+--  end read only
