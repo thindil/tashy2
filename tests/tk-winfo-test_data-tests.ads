8757356a3cd6c99d83e1acf6b67479c45bb7346e
thindil <thindil@laeran.pl>
2021-07-15 05:10:06 +0000

    updated unit tests
    FossilOrigin-Name: 72e28608d669687d793469f16981646aecb55ad4512b12850638ac15340e96f1

--- tests/tk-winfo-test_data-tests.ads
+++ tests/tk-winfo-test_data-tests.ads
@@ -11,43 +11,46 @@ package Tk.Winfo.Test_Data.Tests is
    null record;
 
    procedure Test_Atom_8a3ab9_da7d59(Gnattest_T: in out Test);
-   --  tk-winfo.ads:25:4:Atom:Test_Winfo_Atom
+   --  tk-winfo.ads:26:4:Atom:Test_Winfo_Atom
 
    procedure Test_Atom_Name_ffa709_5ba3e0(Gnattest_T: in out Test);
-   --  tk-winfo.ads:31:4:Atom_Name:Test_Winfo_Atom_Name
+   --  tk-winfo.ads:32:4:Atom_Name:Test_Winfo_Atom_Name
 
    procedure Test_Cells_13d387_f11487(Gnattest_T: in out Test);
-   --  tk-winfo.ads:36:4:Cells:Test_Winfo_Cells
+   --  tk-winfo.ads:37:4:Cells:Test_Winfo_Cells
 
    procedure Test_Children_87e2ce_9a2e0c(Gnattest_T: in out Test);
-   --  tk-winfo.ads:40:4:Children:Test_Winfo_Children
+   --  tk-winfo.ads:41:4:Children:Test_Winfo_Children
 
    procedure Test_Class_1df3af_fa80a1(Gnattest_T: in out Test);
-   --  tk-winfo.ads:44:4:Class:Test_Winfo_Class
+   --  tk-winfo.ads:45:4:Class:Test_Winfo_Class
 
    procedure Test_Color_Map_Full_032f44_b01906(Gnattest_T: in out Test);
-   --  tk-winfo.ads:48:4:Color_Map_Full:Test_Winfo_Color_Map_Full
+   --  tk-winfo.ads:49:4:Color_Map_Full:Test_Winfo_Color_Map_Full
 
    procedure Test_Containing_ed034e_1e0939(Gnattest_T: in out Test);
-   --  tk-winfo.ads:52:4:Containing:Test_Winfo_Containing
+   --  tk-winfo.ads:53:4:Containing:Test_Winfo_Containing
 
    procedure Test_Colors_Depth_27cc4a_1900bc(Gnattest_T: in out Test);
-   --  tk-winfo.ads:57:4:Colors_Depth:Test_Winfo_Depth
+   --  tk-winfo.ads:58:4:Colors_Depth:Test_Winfo_Depth
 
    procedure Test_Exists_e0ba6a_c15285(Gnattest_T: in out Test);
-   --  tk-winfo.ads:61:4:Exists:Test_Winfo_Exists
+   --  tk-winfo.ads:62:4:Exists:Test_Winfo_Exists
 
    procedure Test_Floating_Point_Pixels_e16a1c_9b10a0(Gnattest_T: in out Test);
-   --  tk-winfo.ads:67:4:Floating_Point_Pixels:Test_Winfo_Floating_Point_Pixels
+   --  tk-winfo.ads:68:4:Floating_Point_Pixels:Test_Winfo_Floating_Point_Pixels
 
    procedure Test_Geometry_74d873_9e4b51(Gnattest_T: in out Test);
-   --  tk-winfo.ads:73:4:Geometry:Test_Winfo_Geometry
+   --  tk-winfo.ads:74:4:Geometry:Test_Winfo_Geometry
 
    procedure Test_Height_b50988_c88298(Gnattest_T: in out Test);
-   --  tk-winfo.ads:78:4:Height:Test_Winfo_Height
+   --  tk-winfo.ads:79:4:Height:Test_Winfo_Height
 
    procedure Test_Id_e14e13_d4cbde(Gnattest_T: in out Test);
-   --  tk-winfo.ads:82:4:Id:Test_Winfo_Id
+   --  tk-winfo.ads:83:4:Id:Test_Winfo_Id
+
+   procedure Test_Interpreters_aa8c94_00a874(Gnattest_T: in out Test);
+   --  tk-winfo.ads:87:4:Interpreters:Test_Winfo_Id
 
 end Tk.Winfo.Test_Data.Tests;
 --  end read only
