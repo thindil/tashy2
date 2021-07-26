c948f64d8560534cb967a00b433f49b61adcd6bf
thindil <thindil@laeran.pl>
2021-07-26 05:21:23 +0000

    updated unit tests
    FossilOrigin-Name: 6beb24c1d9427513b8eab4e3ac73a65a9fa3ed37bef44307c97bf6ae261e981f

--- tests/tk-winfo-test_data-tests.ads
+++ tests/tk-winfo-test_data-tests.ads
@@ -44,117 +44,117 @@ package Tk.Winfo.Test_Data.Tests is
    --  tk-winfo.ads:404:4:Geometry:Test_Winfo_Geometry
 
    procedure Test_Height_b50988_c88298(Gnattest_T: in out Test);
-   --  tk-winfo.ads:425:4:Height:Test_Winfo_Height
+   --  tk-winfo.ads:427:4:Height:Test_Winfo_Height
 
    procedure Test_Id_e14e13_d4cbde(Gnattest_T: in out Test);
-   --  tk-winfo.ads:446:4:Id:Test_Winfo_Id
+   --  tk-winfo.ads:448:4:Id:Test_Winfo_Id
 
    procedure Test_Interpreters_aa8c94_a1d488(Gnattest_T: in out Test);
-   --  tk-winfo.ads:473:4:Interpreters:Test_Winfo_Interpreters
+   --  tk-winfo.ads:475:4:Interpreters:Test_Winfo_Interpreters
 
    procedure Test_Is_Mapped_f5e47a_3d3a78(Gnattest_T: in out Test);
-   --  tk-winfo.ads:494:4:Is_Mapped:Test_Winfo_Is_Mapped
+   --  tk-winfo.ads:496:4:Is_Mapped:Test_Winfo_Is_Mapped
 
    procedure Test_Manager_0c5ddb_cd7633(Gnattest_T: in out Test);
-   --  tk-winfo.ads:519:4:Manager:Test_Winfo_Manager
+   --  tk-winfo.ads:521:4:Manager:Test_Winfo_Manager
 
    procedure Test_Name_0da7b9_ba0234(Gnattest_T: in out Test);
-   --  tk-winfo.ads:543:4:Name:Test_Winfo_Name
+   --  tk-winfo.ads:545:4:Name:Test_Winfo_Name
 
    procedure Test_Parent_252286_2c8b1b(Gnattest_T: in out Test);
-   --  tk-winfo.ads:567:4:Parent:Test_Winfo_Parent
+   --  tk-winfo.ads:569:4:Parent:Test_Winfo_Parent
 
    procedure Test_Path_Name_c74192_a4034c(Gnattest_T: in out Test);
-   --  tk-winfo.ads:596:4:Path_Name:Test_Winfo_Path_Name
+   --  tk-winfo.ads:598:4:Path_Name:Test_Winfo_Path_Name
 
    procedure Test_Pixels_60cc8c_ac6879(Gnattest_T: in out Test);
-   --  tk-winfo.ads:621:4:Pixels:Test_Winfo_Pixels
+   --  tk-winfo.ads:623:4:Pixels:Test_Winfo_Pixels
 
    procedure Test_Pointer_X_54c935_e5ce32(Gnattest_T: in out Test);
-   --  tk-winfo.ads:644:4:Pointer_X:Test_Winfo_Pointer_X
+   --  tk-winfo.ads:646:4:Pointer_X:Test_Winfo_Pointer_X
 
    procedure Test_Pointer_X_Y_e6a4c1_ad38ab(Gnattest_T: in out Test);
-   --  tk-winfo.ads:668:4:Pointer_X_Y:Test_Winfo_Pointer_X_Y
+   --  tk-winfo.ads:670:4:Pointer_X_Y:Test_Winfo_Pointer_X_Y
 
    procedure Test_Pointer_Y_37e90e_63e9b1(Gnattest_T: in out Test);
-   --  tk-winfo.ads:691:4:Pointer_Y:Test_Winfo_Pointer_Y
+   --  tk-winfo.ads:693:4:Pointer_Y:Test_Winfo_Pointer_Y
 
    procedure Test_Requested_Height_dc94e9_a6c984(Gnattest_T: in out Test);
-   --  tk-winfo.ads:713:4:Requested_Height:Test_Winfo_Requested_Height
+   --  tk-winfo.ads:715:4:Requested_Height:Test_Winfo_Requested_Height
 
    procedure Test_Requested_Width_701192_2835fe(Gnattest_T: in out Test);
-   --  tk-winfo.ads:735:4:Requested_Width:Test_Winfo_Requested_Width
+   --  tk-winfo.ads:737:4:Requested_Width:Test_Winfo_Requested_Width
 
    procedure Test_Rgb_b213e0_714681(Gnattest_T: in out Test);
-   --  tk-winfo.ads:756:4:Rgb:Test_Winfo_Rgb
+   --  tk-winfo.ads:758:4:Rgb:Test_Winfo_Rgb
 
    procedure Test_Root_X_a9154b_0f2cf6(Gnattest_T: in out Test);
-   --  tk-winfo.ads:779:4:Root_X:Test_Winfo_Root_X
+   --  tk-winfo.ads:781:4:Root_X:Test_Winfo_Root_X
 
    procedure Test_Root_Y_b2f35d_2266c1(Gnattest_T: in out Test);
-   --  tk-winfo.ads:802:4:Root_Y:Test_Winfo_Root_Y
+   --  tk-winfo.ads:804:4:Root_Y:Test_Winfo_Root_Y
 
    procedure Test_Screen_6fc5d3_0a76bf(Gnattest_T: in out Test);
-   --  tk-winfo.ads:822:4:Screen:Test_Winfo_Screen
+   --  tk-winfo.ads:824:4:Screen:Test_Winfo_Screen
 
    procedure Test_Screen_Cells_37d289_147863(Gnattest_T: in out Test);
-   --  tk-winfo.ads:849:4:Screen_Cells:Test_Winfo_Screen_Cells
+   --  tk-winfo.ads:851:4:Screen_Cells:Test_Winfo_Screen_Cells
 
    procedure Test_Screen_Depth_60dc8d_a34485(Gnattest_T: in out Test);
-   --  tk-winfo.ads:872:4:Screen_Depth:Test_Winfo_Screen_Depth
+   --  tk-winfo.ads:874:4:Screen_Depth:Test_Winfo_Screen_Depth
 
    procedure Test_Screen_Height_0d8e96_07a2f4(Gnattest_T: in out Test);
-   --  tk-winfo.ads:895:4:Screen_Height:Test_Winfo_Screen_Height
+   --  tk-winfo.ads:897:4:Screen_Height:Test_Winfo_Screen_Height
 
    procedure Test_Screen_Milimeters_Height_64f6ac_98364e
      (Gnattest_T: in out Test);
-   --  tk-winfo.ads:918:4:Screen_Milimeters_Height:Test_Winfo_Screen_Milimeters_Height
+   --  tk-winfo.ads:920:4:Screen_Milimeters_Height:Test_Winfo_Screen_Milimeters_Height
 
    procedure Test_Screen_Milimeters_Width_aa8ee0_a6bf9c
      (Gnattest_T: in out Test);
-   --  tk-winfo.ads:942:4:Screen_Milimeters_Width:Test_Winfo_Screen_Milimeters_Width
+   --  tk-winfo.ads:944:4:Screen_Milimeters_Width:Test_Winfo_Screen_Milimeters_Width
 
    procedure Test_Screen_Visual_fa94d0_3e55fd(Gnattest_T: in out Test);
-   --  tk-winfo.ads:967:4:Screen_Visual:Test_Winfo_Screen_Visual
+   --  tk-winfo.ads:969:4:Screen_Visual:Test_Winfo_Screen_Visual
 
    procedure Test_Screen_Width_8321ec_7e95f9(Gnattest_T: in out Test);
-   --  tk-winfo.ads:997:4:Screen_Width:Test_Winfo_Screen_Width
+   --  tk-winfo.ads:999:4:Screen_Width:Test_Winfo_Screen_Width
 
    procedure Test_Server_e424b7_51e986(Gnattest_T: in out Test);
-   --  tk-winfo.ads:1017:4:Server:Test_Winfo_Server
+   --  tk-winfo.ads:1019:4:Server:Test_Winfo_Server
 
    procedure Test_Toplevel_5fee9d_f90ff3(Gnattest_T: in out Test);
-   --  tk-winfo.ads:1040:4:Toplevel:Test_Winfo_Toplevel
+   --  tk-winfo.ads:1042:4:Toplevel:Test_Winfo_Toplevel
 
    procedure Test_Viewable_dc7973_b838da(Gnattest_T: in out Test);
-   --  tk-winfo.ads:1063:4:Viewable:Test_Winfo_Viewable
+   --  tk-winfo.ads:1065:4:Viewable:Test_Winfo_Viewable
 
    procedure Test_Visual_Id_bfbe6b_3ec6f0(Gnattest_T: in out Test);
-   --  tk-winfo.ads:1086:4:Visual_Id:Test_Winfo_Visual_Id
+   --  tk-winfo.ads:1088:4:Visual_Id:Test_Winfo_Visual_Id
 
    procedure Test_Visuals_Available_a6aadc_de5564(Gnattest_T: in out Test);
-   --  tk-winfo.ads:1110:4:Visuals_Available:Test_Winfo_Visuals_Available
+   --  tk-winfo.ads:1112:4:Visuals_Available:Test_Winfo_Visuals_Available
 
    procedure Test_Virtual_Root_Height_c10eb6_0cf3d9(Gnattest_T: in out Test);
-   --  tk-winfo.ads:1136:4:Virtual_Root_Height:Test_Winfo_Virtual_Root_Height
+   --  tk-winfo.ads:1138:4:Virtual_Root_Height:Test_Winfo_Virtual_Root_Height
 
    procedure Test_Virtual_Root_Width_34af8b_13e28c(Gnattest_T: in out Test);
-   --  tk-winfo.ads:1160:4:Virtual_Root_Width:Test_Winfo_Virtual_Root_Width
+   --  tk-winfo.ads:1162:4:Virtual_Root_Width:Test_Winfo_Virtual_Root_Width
 
    procedure Test_Virtual_Root_X_6cfaed_207bc9(Gnattest_T: in out Test);
-   --  tk-winfo.ads:1165:4:Virtual_Root_X:Test_Winfo_Virtual_Root_X
+   --  tk-winfo.ads:1187:4:Virtual_Root_X:Test_Winfo_Virtual_Root_X
 
    procedure Test_Virtual_Root_Y_6a3724_e36110(Gnattest_T: in out Test);
-   --  tk-winfo.ads:1169:4:Virtual_Root_Y:Test_Winfo_Virtual_Root_Y
+   --  tk-winfo.ads:1212:4:Virtual_Root_Y:Test_Winfo_Virtual_Root_Y
 
    procedure Test_Width_fa2186_53745f(Gnattest_T: in out Test);
-   --  tk-winfo.ads:1173:4:Width:Test_Winfo_Width
+   --  tk-winfo.ads:1234:4:Width:Test_Winfo_Width
 
    procedure Test_X_91dd91_776ecc(Gnattest_T: in out Test);
-   --  tk-winfo.ads:1177:4:X:Test_Winfo_X
+   --  tk-winfo.ads:1257:4:X:Test_Winfo_X
 
    procedure Test_Y_07f0ff_4d9e51(Gnattest_T: in out Test);
-   --  tk-winfo.ads:1181:4:Y:Test_Winfo_Y
+   --  tk-winfo.ads:1280:4:Y:Test_Winfo_Y
 
 end Tk.Winfo.Test_Data.Tests;
 --  end read only
