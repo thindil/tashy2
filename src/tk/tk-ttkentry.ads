dac3ab0615787a8e656ba84b75a23a8d9fb44296
thindil <thindil@laeran.pl>
2021-07-27 04:45:34 +0000

    started work on binding for Tk ttk::entry widget
    FossilOrigin-Name: df0d06f9c175f7ef0f500dabd821a5528c3564f4d390dff2202ed581034c40e3

---
+++ src/tk/tk-ttkentry.ads
@@ -0,0 +1,45 @@
+-- Copyright (c) 2021 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+with Tcl.Strings; use Tcl.Strings;
+with Tk.Widget; use Tk.Widget;
+with Tk.TtkWidget; use Tk.TtkWidget;
+
+-- ****h* Tk/TtkEntry
+-- FUNCTION
+-- Provides code for manipulate Tk widget ttk::entry
+-- SOURCE
+package Tk.TtkEntry is
+-- ****
+
+   --## rule off REDUCEABLE_SCOPE
+   -- ****t* TtkEntry/TtkEntry.Ttk_Entry
+   -- FUNCTION
+   -- The Tk identifier of the ttk::entry
+   -- HISTORY
+   -- 8.6.0 - Added
+   -- SOURCE
+   subtype Ttk_Entry is Ttk_Widget;
+   -- ****
+
+   type Ttk_Entry_Options is new Ttk_Widget_Options with record
+      X_Scroll_Command: Tcl_String;
+      Export_Selection: Extended_Boolean;
+      Invalid_Command: Tcl_String;
+      Justify: Justify_Type;
+      Show: Boolean := True;
+   end record;
+
+   --## rule on REDUCEABLE_SCOPE
+end Tk.TtkEntry;
