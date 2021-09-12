cda4c3fe78ba760728804b3088e83f864ad1cbee
thindil <thindil@laeran.pl>
2021-09-12 05:11:20 +0000

    continue work on getting image for Key_Syms_Type
    FossilOrigin-Name: 965e99aad23a94da1919df8b46b0b45be39d0169b9d54fa84337745d525b47e9

--- src/tk/tk-bind.adb
+++ src/tk/tk-bind.adb
@@ -53,7 +53,8 @@ package body Tk.Bind is
          when KEY_ARABIC_COMMA .. KEY_SERBIAN_DZE |
            KEY_CYRILLIC_YU .. KEY_CYRILLIC_HARDSIGN =>
             Image(5) := To_Upper(Image(5));
-         when SHIFT_KEY_SERBIAN_DJE .. SHIFT_KEY_SERBIAN_DZE =>
+         when SHIFT_KEY_SERBIAN_DJE .. SHIFT_KEY_SERBIAN_DZE |
+           SHIFT_KEY_CYRILLIC_YU .. SHIFT_KEY_CYRILLIC_HARDSIGN =>
             Image(11) := To_Upper(Image(11));
             Start_Index := Index(Image, "_", Backward);
             Image(Start_Index .. Image'Last) :=
