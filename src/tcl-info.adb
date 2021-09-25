dcf285dde8bc907ef3416cb974f4b64a5346579b
thindil <thindil@laeran.pl>
2021-09-25 04:03:36 +0000

    updated code to the new version of Tcl_Eval with String result
    FossilOrigin-Name: 313cda4b16e408f4d57442313898d651f9bb62d973206310f429121958e9c9d8

--- src/tcl-info.adb
+++ src/tcl-info.adb
@@ -16,12 +16,10 @@ package body Tcl.Info is
 
    function Get_Commands_Count
      (Interpreter: Tcl_Interpreter := Get_Interpreter) return Natural is
-      function Integer_Eval is new Generic_Scalar_Tcl_Eval
-        (Result_Type => Integer);
    begin
       return
-        Integer_Eval
-          (Tcl_Script => "info cmdcount", Interpreter => Interpreter);
+        Tcl_Eval
+          (Tcl_Script => "info cmdcount", Interpreter => Interpreter).Result;
    end Get_Commands_Count;
 
 end Tcl.Info;
