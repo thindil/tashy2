c5ded8c37c2e6a86ec3886bee010fa5c017ad5cc
thindil <thindil@laeran.pl>
2021-10-01 04:36:52 +0000

    bring back SPARK_Mode or the library will not compile
    FossilOrigin-Name: 611af4e77bf20deb0cffe8d3b5fc37e93aa8ec64c881cecd5f96cd7d23bcf991

--- src/tcl-lists.ads
+++ src/tcl-lists.ads
@@ -19,7 +19,9 @@ with Tcl.Variables;
 -- FUNCTION
 -- Provide code to manipulate the Tcl lists.
 -- SOURCE
-package Tcl.Lists is
+package Tcl.Lists with
+   SPARK_Mode
+is
 -- ****
 
    --## rule off REDUCEABLE_SCOPE
