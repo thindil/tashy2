a3e992d778c60ecd31fc8bce68896e496a583345
thindil <thindil@laeran.pl>
2021-10-06 05:15:07 +0000

    updated unit tests
    FossilOrigin-Name: 2109a7c582608bc94700cf01bec5b5807ec5b76f502a65d5ee1c90f1a121e261

--- tests/tk-bind-test_data.adb
+++ tests/tk-bind-test_data.adb
@@ -10,12 +10,16 @@ package body Tk.Bind.Test_Data is
 
    procedure Set_Up(Gnattest_T: in out Test) is
       pragma Unreferenced(Gnattest_T);
+      Interp: constant Tcl_Interpreter := Create_Interpreter;
    begin
       if Value("DISPLAY", "")'Length = 0
         or else Info.Exists("tk_version").Result then
          return;
       end if;
-      Tk_Init;
+      Set_Interpreter(Interp);
+      if Tk_Init(Interp) = TCL_OK then
+         return;
+      end if;
    end Set_Up;
 
    procedure Tear_Down(Gnattest_T: in out Test) is
