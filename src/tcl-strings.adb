f31b82b4b9d1b4e17e7bc654edb71509e646aed7
thindil <thindil@laeran.pl>
2021-10-16 03:42:21 +0000

    fixed some SPARK errors
    FossilOrigin-Name: a45c9fc89f3cf5e250dc9e2acccaf90492c0b151760233d26af3829b99a99671

--- src/tcl-strings.adb
+++ src/tcl-strings.adb
@@ -34,7 +34,14 @@ package body Tcl.Strings is
          end if;
       end Truncate_New_String;
    begin
-      Append(Source => New_String, New_Item => Source);
+      if Source'Length <= Natural'Last - Length(New_String) then
+         Append(Source => New_String, New_Item => Source);
+      else
+         Append
+           (Source => New_String,
+            New_Item =>
+              Source(Source'First .. Natural'Last - Length(New_String)));
+      end if;
       if Index(Source => New_String, Pattern => " ") = 0 or
         Source'Length < 2 then
          return New_String;
