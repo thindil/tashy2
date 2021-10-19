9c4d2ff563df079e39425130a0f23c580ff3114d
thindil <thindil@laeran.pl>
2021-10-19 04:20:13 +0000

    fixed some AdaControl errors
    FossilOrigin-Name: e5d8756fe078717e4762873c98cd0f29eba616265aef9f076b04d143a287f71a

--- src/tk/tk-frame.ads
+++ src/tk/tk-frame.ads
@@ -163,8 +163,7 @@ package Tk.Frame is
       Pre'Class =>
       (Path_Name'Length > 0
        and then
-         Long_Long_Integer
-           (Path_Name'Length + Options_To_String(Options => Options)'Length) <=
+           Path_Name'Length + Options_To_String(Options => Options)'Length <=
          Long_Long_Integer(Natural'Last) - 7) and
       Interpreter /= Null_Interpreter,
       Test_Case => (Name => "Test_Create_Frame1", Mode => Nominal);
