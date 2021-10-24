ebb6203c07bb5619fed0caf436bc64a0edd848dc
thindil <thindil@laeran.pl>
2021-10-24 04:18:15 +0000

    style: fixed some AdaControl errors
    FossilOrigin-Name: 0b0b942d9cae5836867f6a41b055d4c1052d66a10371cd24dde556b9345aa688

--- src/tk/tk-button.adb
+++ src/tk/tk-button.adb
@@ -131,10 +131,12 @@ package body Tk.Button is
       return Get_Widget(Path_Name => Path_Name, Interpreter => Interpreter);
    end Create;
 
+   --## rule off IMPROPER_INITIALIZATION
    procedure Create
      (Button_Widget: out Tk_Button; Path_Name: Tk_Path_String;
       Options: Button_Options;
       Interpreter: Tcl_Interpreter := Get_Interpreter) is
+   --## rule on IMPROPER_INITIALIZATION
    begin
       Button_Widget :=
         Create
