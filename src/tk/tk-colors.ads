57f71a4741fd088976e4601e23fb1acea43942eb
thindil <thindil@laeran.pl>
2021-11-06 05:17:12 +0000

    feat: started work on Tk.Colors package
    FossilOrigin-Name: 6b14ff88d84d7eaa7acfb6f208e440c723dd251bc96f63a746955146307a96c7

---
+++ src/tk/tk-colors.ads
@@ -0,0 +1,26 @@
+-- Copyright (c) 2021 Bartek thindil Jasicki <thindil@laeran.pl>
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+package Tk.Colors is
+
+   --## rule off REDUCEABLE_SCOPE
+   --## rule off NAMING_CONVENTION
+   type Colors is
+     (ALICE_BLUE, ANTIQUE_WHITE, ANTIQUE_WHITE1, ANTIQUE_WHITE2,
+      ANTIQUE_WHITE3, ANTIQUE_WHITE4, AQUA, AQUAMARINE, AQUAMARINE1,
+      AQUAMARINE2, AQUAMARINE3, AQUAMARINE4);
+   --## rule on NAMING_CONVENTION
+   --## rule on REDUCEABLE_SCOPE
+
+end Tk.Colors;
