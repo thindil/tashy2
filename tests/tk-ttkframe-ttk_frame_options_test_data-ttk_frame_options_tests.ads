307c8bdf831e36071300189ecc168fed0edf29b4
thindil <thindil@laeran.pl>
2021-12-02 05:45:49 +0000

    tests: updated unit tests
    FossilOrigin-Name: 7b658a48c8a3772d67f9273c6583b32bbfcdcde3ac6580da149d7acd92232205

--- tests/tk-ttkframe-ttk_frame_options_test_data-ttk_frame_options_tests.ads
+++ tests/tk-ttkframe-ttk_frame_options_test_data-ttk_frame_options_tests.ads
@@ -26,11 +26,11 @@ package Tk.TtkFrame.Ttk_Frame_Options_Test_Data.Ttk_Frame_Options_Tests is
 
    procedure Test_Create_ebbdc1_471f4e
      (Gnattest_T: in out Test_Ttk_Frame_Options);
-   --  tk-ttkframe.ads:158:4:Create:Test_Create_Frame2
+   --  tk-ttkframe.ads:153:4:Create:Test_Create_Frame2
 
    procedure Test_Get_Options_ded36e_ce77f0
      (Gnattest_T: in out Test_Ttk_Frame_Options);
-   --  tk-ttkframe.ads:187:4:Get_Options:Test_Get_Options_Frame
+   --  tk-ttkframe.ads:177:4:Get_Options:Test_Get_Options_Frame
 
 end Tk.TtkFrame.Ttk_Frame_Options_Test_Data.Ttk_Frame_Options_Tests;
 --  end read only
