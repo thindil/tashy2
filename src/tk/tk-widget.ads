1e0468095a197a377f6612e7aa411becfece6d0e
thindil <thindil@laeran.pl>
2021-12-11 05:43:14 +0000

    tests: added precondition check for Command_Name to Execute_Widget_Command
    FossilOrigin-Name: c167f7dee17af5f45425ca5819382fc74a07ffdedc41c11dcaa5da02602f8847

--- src/tk/tk-widget.ads
+++ src/tk/tk-widget.ads
@@ -979,6 +979,7 @@ package Tk.Widget is
       Options: String := "") with
       Global => null,
       Pre => (Widgt /= Null_Widget and Command_Name'Length > 0)
+      and then Name_Is_Valid(Command_Name)
       and then
         Long_Long_Integer(Tk_Path_Name(Widgt => Widgt)'Length) +
           Long_Long_Integer(Command_Name'Length) +
@@ -1012,6 +1013,7 @@ package Tk.Widget is
       return Tcl_String_Result with
       Global => null,
       Pre => (Widgt /= Null_Widget and Command_Name'Length > 0)
+      and then Name_Is_Valid(Command_Name)
       and then
         Long_Long_Integer(Tk_Path_Name(Widgt => Widgt)'Length) +
           Long_Long_Integer(Command_Name'Length) +
