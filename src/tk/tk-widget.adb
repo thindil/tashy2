63ed6b2551cf67aece5d0f4d3de90ee61af272c0
thindil <thindil@laeran.pl>
2021-12-15 05:00:27 +0000

    feat: added better checks for correctness of Option_Image for Tcl_String
    FossilOrigin-Name: a3774ad0419fe1fd71f1cc9fa2d919bef935ac31583174e5c73018ee561e2a64

--- src/tk/tk-widget.adb
+++ src/tk/tk-widget.adb
@@ -123,6 +123,15 @@ package body Tk.Widget is
      (Name: Variable_Name; Value: Tcl_String;
       Options_String: in out Unbounded_String) is
    begin
+      if not Name_Is_Valid(Name => Name) then
+         return;
+      end if;
+      if Long_Long_Integer(Length(Source => Options_String)) +
+        Long_Long_Integer(Length(Source => Value)) +
+        Long_Long_Integer(Name'Length) + 3 >
+        Long_Long_Integer(Positive'Last) then
+         return;
+      end if;
       if Length(Source => Value) > 0 then
          Append
            (Source => Options_String,
