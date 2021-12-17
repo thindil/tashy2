65f6134a9f1c07ef91e95588f530859262785a86
thindil <thindil@laeran.pl>
2021-12-17 04:48:44 +0000

    tests: added better precondition for Option_Image for Pixel_Data
    FossilOrigin-Name: bf08a1aca12d24d74e3be94db95eb290a74f9a2554cd49b484d85246397dcbee

--- src/tk/tk-widget.ads
+++ src/tk/tk-widget.ads
@@ -740,7 +740,12 @@ package Tk.Widget is
    procedure Option_Image
      (Name: Variable_Name; Value: Pixel_Data;
       Options_String: in out Unbounded_String) with
-      Pre => Name_Is_Valid(Name => Name),
+      Pre => Name_Is_Valid(Name => Name)
+      and then
+        Long_Long_Integer(Length(Source => Options_String)) +
+          Long_Long_Integer(Pixel_Data_Image(Value)'Length) +
+          Long_Long_Integer(Name'Length) + 3 <=
+        Long_Long_Integer(Positive'Last),
       Test_Case => (Name => "Test_Option_Image_Pixed_Data", Mode => Nominal);
    procedure Option_Image
      (Name: Variable_Name; Value: Relief_Type;
