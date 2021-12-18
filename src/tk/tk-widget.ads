35bcfa3c36baa799e4d8a0e07d0b5f6b54191587
thindil <thindil@laeran.pl>
2021-12-18 04:45:23 +0000

    tests: added better precondition for Option_Image for Relief_Type
    FossilOrigin-Name: 95a43264cb6eaead76035e76dc8a0e7469dd1175b106df0fc5f23b1b9dca922e

--- src/tk/tk-widget.ads
+++ src/tk/tk-widget.ads
@@ -750,7 +750,12 @@ package Tk.Widget is
    procedure Option_Image
      (Name: Variable_Name; Value: Relief_Type;
       Options_String: in out Unbounded_String) with
-      Pre => Name_Is_Valid(Name => Name),
+      Pre => Name_Is_Valid(Name => Name)
+      and then
+        Long_Long_Integer(Length(Source => Options_String)) +
+          Long_Long_Integer(Relief_Type'Image(Value)'Length) +
+          Long_Long_Integer(Name'Length) + 3 <=
+        Long_Long_Integer(Positive'Last),
       Test_Case => (Name => "Test_Option_Image_Relief_Type", Mode => Nominal);
    procedure Option_Image
      (Name: Variable_Name; Value: State_Type;
