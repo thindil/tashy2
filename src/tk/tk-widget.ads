bdbbb4a7934e6dd25aef2ba2fa4e6bb2abf02813
thindil <thindil@laeran.pl>
2021-12-27 04:33:43 +0000

    tests: added better precondition for Option_Image for Extended_Boolean
    FossilOrigin-Name: df9f05df97d53759fa638b25aad52f111f6960ab89e4834982c69dd3ab0d96cc

--- src/tk/tk-widget.ads
+++ src/tk/tk-widget.ads
@@ -845,7 +845,12 @@ package Tk.Widget is
    procedure Option_Image
      (Name: Variable_Name; Value: Extended_Boolean;
       Options_String: in out Unbounded_String) with
-      Pre => Name_Is_Valid(Name => Name),
+      Global => null,
+      Pre => Name_Is_Valid(Name => Name)
+      and then
+        Long_Long_Integer(Length(Source => Options_String)) +
+          Long_Long_Integer(Name'Length) + 4 <=
+        Long_Long_Integer(Positive'Last),
       Test_Case => (Name => "Test_Option_Image_Extended_Boolean",
        Mode => Nominal);
    procedure Option_Image
