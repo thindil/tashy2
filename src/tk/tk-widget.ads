0f502ac1240b0f2aab0462f443da5ef395c1f012
thindil <thindil@laeran.pl>
2021-12-29 05:01:32 +0000

    tests: added better precondition for Option_Image for Anchor_Directions
    FossilOrigin-Name: d8a1ff2147b7e5efbae1758591bfded57c802910a0cb57df8fcd9adba6890553

--- src/tk/tk-widget.ads
+++ src/tk/tk-widget.ads
@@ -877,7 +877,13 @@ package Tk.Widget is
    procedure Option_Image
      (Name: Variable_Name; Value: Anchor_Directions;
       Options_String: in out Unbounded_String) with
-      Pre => Name_Is_Valid(Name => Name),
+      Global => null,
+      Pre => Name_Is_Valid(Name => Name)
+      and then
+        Long_Long_Integer(Length(Source => Options_String)) +
+          Long_Long_Integer(Anchor_Directions'Image(Value)'Length) +
+          Long_Long_Integer(Name'Length) + 3 <=
+        Long_Long_Integer(Positive'Last),
       Test_Case => (Name => "Test_Option_Image_Anchor_Directions",
        Mode => Nominal);
    procedure Option_Image
