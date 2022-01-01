86bf723aef91f5a2af8b031587ba1800b78bf45f
thindil <thindil@laeran.pl>
2022-01-01 04:47:26 +0000

    tests: added better precondition for Option_Image for Boolean
    FossilOrigin-Name: 5617cf03a12fd1feb4a8e00ecc7d4f5d6beba6c29e9b425a4a45510026b23661

--- src/tk/tk-widget.ads
+++ src/tk/tk-widget.ads
@@ -915,7 +915,12 @@ package Tk.Widget is
    procedure Option_Image
      (Name: Variable_Name; Value: Boolean;
       Options_String: in out Unbounded_String) with
-      Pre => Name_Is_Valid(Name => Name),
+      Global => null,
+      Pre => Name_Is_Valid(Name => Name)
+      and then
+        Long_Long_Integer(Length(Source => Options_String)) +
+          Long_Long_Integer(Name'Length) + 2 <=
+        Long_Long_Integer(Positive'Last),
       Test_Case => (Name => "Test_Option_Image_Boolean", Mode => Nominal);
    procedure Option_Image
      (Name: Variable_Name; Value: Color_Type;
