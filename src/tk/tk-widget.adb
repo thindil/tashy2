db403bc93eab93e6ea8231015cfc126eb0881dc1
thindil <thindil@laeran.pl>
2022-01-09 05:21:04 +0000

    fix: better check for length of the Name argument for Option_Value for Direction_Type
    FossilOrigin-Name: 1fab1f9ea1d6ab16400a82e4204ed1257926dc44e5e8cb3dd2d4125df6683e61

--- src/tk/tk-widget.adb
+++ src/tk/tk-widget.adb
@@ -661,7 +661,7 @@ package body Tk.Widget is
    end Option_Value;
 
    function Option_Value
-     (Widgt: Tk_Widget; Name: String) return Directions_Type is
+     (Widgt: Tk_Widget; Name: Variable_Name) return Directions_Type is
       Result: constant String :=
         Execute_Widget_Command
           (Widgt => Widgt, Command_Name => "cget", Options => "-" & Name)
