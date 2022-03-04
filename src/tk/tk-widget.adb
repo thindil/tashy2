11b69d854ed50fbb10c310f4c678c46ba3c6f3cf
thindil <thindil@laeran.pl>
2022-03-04 04:26:55 +0000

    feat: better check for length of returned value for Option_Image for Extended_Boolean
    FossilOrigin-Name: 0b12abeb63fc7fb54d54058f8cfd6e3cd676573edddeb0913b5cad302b20162f

--- src/tk/tk-widget.adb
+++ src/tk/tk-widget.adb
@@ -395,9 +395,9 @@ package body Tk.Widget is
       if not Name_Is_Valid(Name => Name) then
          return;
       end if;
-      if Long_Long_Integer(Length(Source => Options_String)) +
-        Long_Long_Integer(Name'Length) + 4 >
-        Long_Long_Integer(Positive'Last) then
+      if Long_Long_Integer(Length(Source => Options_String)) >
+        Long_Long_Integer(Positive'Last) -
+          (Long_Long_Integer(Name'Length) + 4) then
          return;
       end if;
       if Value = FALSE then
