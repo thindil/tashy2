7210c52fd402284c559500dea5aac5c179046b3e
thindil <thindil@laeran.pl>
2022-03-08 04:29:30 +0000

    feat: better check for length of returned value for Option_Image for Point_Position
    FossilOrigin-Name: cb6f4344fbcc49ab8780e7890d127978310da46926f6ceab57362ad293e9ed35

--- src/tk/tk-widget.adb
+++ src/tk/tk-widget.adb
@@ -505,11 +505,11 @@ package body Tk.Widget is
       if not Name_Is_Valid(Name => Name) then
          return;
       end if;
-      if Long_Long_Integer(Length(Source => Options_String)) +
-        Long_Long_Integer(Extended_Natural'Image(Value.X)'Length) +
-        Long_Long_Integer(Extended_Natural'Image(Value.Y)'Length) +
-        Long_Long_Integer(Name'Length) + 2 >
-        Long_Long_Integer(Positive'Last) then
+      if Long_Long_Integer(Length(Source => Options_String)) >
+        Long_Long_Integer(Positive'Last) -
+          (Long_Long_Integer(Extended_Natural'Image(Value.X)'Length) +
+           Long_Long_Integer(Extended_Natural'Image(Value.Y)'Length) +
+           Long_Long_Integer(Name'Length) + 2) then
          return;
       end if;
       Append
